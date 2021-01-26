bf0d77f1eb5f0775fa4c839c85f68437f847d684
Fang Wei Jie <fangweijie1327@e.newera.edu.my>
2021-01-26 13:12:48 +0800

    Modernized styling of login and register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,50 +6,48 @@ Login -
 
 @section('body')
 <div class="container">
-    <form method="post" action="{{ route('login') }}">
-        @csrf
+    <div class="row justify-content-center">
+        <div class="col-sm-5">
+            <form method="post" action="{{ route('login') }}">
+                @csrf
 
-        @if (session('status'))
-        <div class="alert alert-danger" role="alert">
-            {{ session('status') }}
-        </div>
-        @endif
-
-        <div class="form-group row">
-            <label class="col-sm-2" for="email">Email</label>
-
-            <div class="col-sm-6">
-                <input id="email" class="form-control @error('email') is-invalid @enderror @if(old('email')) is-valid @endif" type="email" name="email" value="{{ old('email') }}" maxlength=75 placeholder="username@email.com">
-                @error('email')
-                <div class="invalid-feedback">
-                    {{ $message }}
+                <div class="row">
+                    <h5>Login</h5>
                 </div>
-                @enderror
-            </div>
-        </div>
 
-        <div class="form-group row">
-            <label class="col-sm-2" for="password">Password</label>
-
-            <div class="col-sm-6">
-                <input id="password" class="form-control @error('password') is-invalid @enderror" type="password" name="password" value="{{ old('password') }}" minlength="8" maxlength=50 placeholder="8 - 25 characters">
-                @error('password')
-                <div class="invalid-feedback">
-                    {{ $message }}
+                @if (session('status'))
+                <div class="alert alert-danger" role="alert">
+                    {{ session('status') }}
                 </div>
-                @enderror
-            </div>
-        </div>
+                @endif
 
-        <div class="form-group row d-flex align-items-center">
-            <div class="col-sm-1">
-                <button class="btn btn-primary" type="submit" name="login">Login</button>
-            </div>
-            <div class="col-sm-9">
-                <input type="checkbox" class="form-check-input" id="keep-signed-in">
-                <label class="form-check-label" for="keep-signed-in">Keep me signed in</label>
-            </div>
+                <div class="row">
+                    <input id="email" class="form-control @error('email') is-invalid @enderror @if(old('email')) is-valid @endif" type="email" name="email" value="{{ old('email') }}" maxlength=255 placeholder="Email address">
+                    @error('email')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                </div>
+
+                <div class="row">
+                    <input id="password" class="form-control @error('password') is-invalid @enderror" type="password" name="password" placeholder="Password">
+                    @error('password')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                </div>
+
+                <div class="row">
+                    <button class="btn btn-primary" type="submit" name="login">Login</button>
+                </div>
+
+                <div class="row">
+                    Don't have an account? <a href="{{ route('register') }}">Register now</a>
+                </div>
+            </form>
         </div>
-    </form>
+    </div>
 </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,70 +6,64 @@ Register -
 
 @section('body')
 <div class="container">
-    <form method="post" action="{{ route('register') }}">
-        @csrf
+    <div class="row justify-content-center">
+        <div class="col-sm-5">
+            <form method="post" action="{{ route('register') }}">
+                @csrf
 
-        <div class="form-group row">
-            <label class="col-sm-2" for="name">Name</label>
-            <div class="col-sm-6">
-                <input id="name" class="form-control @error('name') is-invalid @enderror @if(old('name')) is-valid @endif"  type="text" name="name" value="{{ old('name') }}" maxlength=255 placeholder="Maximum 255 characters">
-                @error('name')
-                <div class="invalid-feedback">
-                    {{ $message }}
+                <div class="row">
+                    <h5>Register</h5>
                 </div>
-                @enderror
-            </div>
-        </div>
 
-        <div class="form-group row">
-            <label class="col-sm-2" for="phone">Mobile Phone</label>
-            <div class="col-sm-6">
-                <input id="phone" class="form-control @error('phone') is-invalid @enderror @if(old('phone')) is-valid @endif" type="tel" name="phone" value="{{ old('phone') }}" maxlength=11 placeholder="01XXXXXXXXX">
-                @error('phone')
-                <div class="invalid-feedback">
-                    {{ $message }}
+                <div class="row">
+                    <input id="name" class="form-control @error('name') is-invalid @enderror @if(old('name')) is-valid @endif" type="text" name="name" value="{{ old('name') }}" maxlength=255 placeholder="Name">
+                    @error('name')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
-                @enderror
-            </div>
-        </div>
 
-        <div class="form-group row">
-            <label class="col-sm-2" for="email">Email</label>
-
-            <div class="col-sm-6">
-                <input id="email" class="form-control @error('email') is-invalid @enderror @if(old('email')) is-valid @endif" type="email" name="email" value="{{ old('email') }}" maxlength=75 placeholder="username@email.com">
-                @error('email')
-                <div class="invalid-feedback">
-                    {{ $message }}
+                <div class="row">
+                    <input id="phone" class="form-control @error('phone') is-invalid @enderror @if(old('phone')) is-valid @endif" type="tel" name="phone" value="{{ old('phone') }}" maxlength=11 placeholder="Mobile Phone Number (01XXXXXXXXX)">
+                    @error('phone')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
-                @enderror
-            </div>
-        </div>
 
-        <div class="form-group row">
-            <label class="col-sm-2" for="password">Password</label>
-
-            <div class="col-sm-6">
-                <input id="password" class="form-control @error('password') is-invalid @enderror" type="password" name="password" value="{{ old('password') }}" minlength="8" maxlength=50 placeholder="8 - 25 characters">
-                @error('password')
-                <div class="invalid-feedback">
-                    {{ $message }}
+                <div class="row">
+                    <input id="email" class="form-control @error('email') is-invalid @enderror @if(old('email')) is-valid @endif" type="email" name="email" value="{{ old('email') }}" maxlength=75 placeholder="Email Address">
+                    @error('email')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
-                @enderror
-            </div>
-        </div>
 
-        <div class="form-group row">
-            <label class="col-sm-2" for="password_confirmation">Confirm Password</label>
+                <div class="row">
+                    <input id="password" class="form-control @error('password') is-invalid @enderror" type="password" name="password" value="{{ old('password') }}" minlength="8" placeholder="Password (Minimum 8 characters)">
+                    @error('password')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                </div>
 
-            <div class="col-sm-6">
-                <input id="password_confirmation" class="form-control  @error('password') is-invalid @enderror" type="password" name="password_confirmation" value="{{ old('password') }}" minlength="8" maxlength=50 placeholder="8 - 25 characters">
-            </div>
-        </div>
+                <div class="row">
+                    <input id="password_confirmation" class="form-control  @error('password') is-invalid @enderror" type="password" name="password_confirmation" value="{{ old('password') }}" minlength="8" placeholder="Retype password again">
+                </div>
 
-        <div class="form-group row">
-            <button class="btn btn-primary" type="submit" name="register">Register</button>
+                <div class="row">
+                    <button class="btn btn-primary" type="submit" name="register">Register</button>
+                </div>
+
+                <div class="row">
+                    Already have an account? <a href="{{ route('login') }}">Login now</a>
+                </div>
+            </form>
         </div>
-    </form>
+    </div>
 </div>
 @endsection
