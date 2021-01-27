2549458c898c1c505f54ca7f8351c2641351b45d
Fang Wei Jie <fangweijie1327@e.newera.edu.my>
2021-01-27 16:09:44 +0800

    Completed myaccount

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AccountController extends Controller
+{
+
+    function __construct()
+    {
+
+        $this -> middleware(['auth']);
+
+    }
+
+    function view () {
+
+        return view ('myaccount');
+
+    }
+
+    function update (Request $request) {
+
+        $user = Auth::user();
+
+        if (isset ($_POST["change-name"]) ) {
+
+            // validation
+            $this -> validate($request, [
+
+                'name' => 'required',
+
+            ]);
+
+            // save changes
+            $user->name = $request->input('name');
+            $user->save();
+
+            // redirect back to page with info prompt
+            return back() -> with('info', 'Name updated');
+
+        } else if (isset ($_POST["change-phone"]) ) {
+
+            // validate
+            $this -> validate($request, [
+
+                'phone' => 'required | min:10 | max:11',
+                'phone-update-password' => 'required',
+
+            ]);
+
+            // check if password matches, if not redirect back with alert prompt
+            if (!Hash::check($request->input('phone-update-password'), $user->password)) {
+
+                return back() -> with('alert', 'Password incorrect. Phone number not updated');
+
+            }
+
+            // save changes
+            $user->phone = $request->input('phone');
+            $user->save();
+
+            // redirect back to page with info prompt
+            return back() -> with('info', 'Phone number updated. ');
+
+        } else if (isset ($_POST["change-email"]) ) {
+
+            // validate
+            $this -> validate($request, [
+
+                'email-update-password' => 'required',
+                'email' => 'required | email | max:255',
+
+            ]);
+
+            // check if password matches, if not redirect back with alert prompt
+            if (!Hash::check($request->input('email-update-password'), $user->password)) {
+
+                return back() -> with('alert', 'Password incorrect. Email not updated');
+
+            }
+
+            // save changes
+            $user->email = $request->input('email');
+            $user->save();
+
+            // redirect back to page with info prompt
+            return back() -> with('info', 'Email updated. ');
+
+        } else if (isset ($_POST["change-password"]) ) {
+
+            // validate
+            $this -> validate($request, [
+
+                'old-password' => 'required',
+                'new-password' => 'required',
+                'confirm-password' => 'required',
+
+            ]);
+
+            // check if password matches, if not redirect back with alert prompt
+            if (!Hash::check($request->input('old-password'), $user->password)) {
+
+                return back() -> with('alert', 'Current password incorrect. Password not updated. ');
+
+            } else {
+
+                // check if new password matches confirm password field, if not redirect back with alert propr
+                if ($request->input('new-password') != $request->input('confirm-password')) {
+
+                    return back() -> with('alert', 'New password and confirm password does not match. Password not updated. ');
+
+                }
+
+            }
+
+            // save changes
+            $user->password = Hash::make($request->input('new-password'));
+            $user->save();
+
+            // redirect back to page with info prompt
+            return back() -> with('info', 'Password updated. ');
+
+        } else if (isset ($_POST["delete-account"]) ) {
+
+            $this -> validate($request, [
+
+                'delete-password' => 'required',
+
+            ]);
+
+            // check if password matches, if not redirect back with alert prompt
+            if (Hash::check($request->input('delete-password'), $user->password)) {
+
+                // empties unnecessary data field
+                $user->name = 'DELETED';
+                $user->phone = '';
+                $user->email = Str::random(255);
+                $user->password = '';
+                $user->remember_token = null;
+                $user->save();
+
+            } else {
+
+                return back() -> with('alert', 'Password incorrect. Account not deleted. ');
+
+            }
+
+            // redirect user out of the current page
+            Auth::logout();
+            return redirect() -> route('login');
+
+        }
+
+    }
+
+}
---
+++ resources/views/myaccount.blade.php
@@ -0,0 +1,280 @@
+@extends('layout.frame')
+
+@section('title')
+My Account -
+@endsection
+
+@section('body')
+<div class="container">
+    <h1>My Account</h1>
+
+    <hr>
+
+    <table class="table table-borderless" width="100vw">
+        <thead>
+            <tr>
+                <th width="45%"></th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    <div class="col">
+                        <h3>Name</h3>
+                        <span>{{ Auth::user()->name }}</span>
+                    </div>
+                </td>
+                <td>
+                    <button type="button" class="btn btn-outline-primary" id="changeNameButton" data-toggle="modal" data-target="#changeName">Change</button>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <div class="col">
+                        <h3>Phone Number</h3>
+                        <span>{{ Auth::user()->phone }}</span>
+                    </div>
+                </td>
+                <td>
+                    <button class="btn btn-outline-primary" id="change-phone-button" data-toggle="modal" data-target="#changePhone">Change</button>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <div class="col">
+                        <h3>Email</h3>
+                        <span>{{ Auth::user()->email }}</span>
+                    </div>
+                </td>
+                <td>
+                    <button class="btn btn-outline-primary" id="change-email-button" data-toggle="modal" data-target="#changeEmail">Change</button>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <div class="col">
+                        <h3>Password</h3>
+                        <span>Change password every few months for security</span>
+                    </div>
+                </td>
+                <td>
+                    <button class="btn btn-outline-primary" id="change-password-button" data-toggle="modal" data-target="#changePassword">Change</button>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <div class="col">
+                        <h3>Delete Account</h3>
+                    </div>
+                </td>
+                <td>
+                    <button type="button" class="btn btn-outline-danger" id="accountDeleteButton" data-toggle="modal" data-target="#accountDeleteConfirmation">
+                        <i class="bi bi-trash-fill"></i>
+                        DELETE ACCOUNT
+                    </button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+    <!-- change name modal view -->
+    <div class="modal fade" id="changeName" tabindex="-1" role="dialog" aria-labelledby="changeNameLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <span class="modal-title" id="titleLabel">Change name</span>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <form action="{{ route('myaccount') }}" method="post">
+                    @csrf
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label>Enter your new name</label>
+                            <input type="text" class="form-control" name="name" maxlength="255" placeholder="Maximum 255 characters" required>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn btn-primary" name="change-name">Change name</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- change phone modal view -->
+    <div class="modal fade" id="changePhone" tabindex="-1" role="dialog" aria-labelledby="changePhoneLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <span class="modal-title" id="titleLabel">Change Phone Number</span>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <form action="{{ route('myaccount') }}" method="post">
+                    @csrf
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label>Enter new phone number</label>
+                            <input type="text" class="form-control" name="phone" minlength="10" maxlength="11" placeholder="Example: 01234567890" required>
+                            <label>Enter password to confirm changes</label>
+                            <input type="password" class="form-control" name="phone-update-password" placeholder="Enter current password" required>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn btn-primary" name="change-phone">Change phone number</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- change email modal view -->
+    <div class="modal fade" id="changeEmail" tabindex="-1" role="dialog" aria-labelledby="changeEmailLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <span class="modal-title" id="titleLabel">Change Email Address</span>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <form action="{{ route('myaccount') }}" method="post">
+                    @csrf
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label>Enter new email address</label>
+                            <input type="email" class="form-control" name="email" maxlength="255" placeholder="example@email.com" required>
+                            <label>Enter password to confirm changes</label>
+                            <input type="password" class="form-control" name="email-update-password" placeholder="Enter current password" required>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn btn-primary" name="change-email">Change email address</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- change passsword modal view -->
+    <div class="modal fade" id="changePassword" tabindex="-1" role="dialog" aria-labelledby="changePasswordLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <span class="modal-title" id="titleLabel">Change Password</span>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <form action="{{ route('myaccount') }}" method="post">
+                    @csrf
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label>Enter current password</label>
+                            <input id="old-password" class="form-control" type="password" name="old-password" placeholder="Enter current password" required><br>
+                            <label>Enter new password</label>
+                            <input id="new-password" class="form-control" type="password" name="new-password" minlength=8 placeholder="Minimum of 8 characters" required><br>
+                            <label>Confirm new password</label>
+                            <input id="confirm-password" class="form-control" type="password" name="confirm-password" minlength=8 placeholder="Retype new password" required>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button class="btn btn-primary" type="submit" name="change-password">Change password</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- account delete confirmation modal view -->
+    <div class="modal fade" id="accountDeleteConfirmation" tabindex="-1" role="dialog" aria-labelledby="accountDeleteConfirmationLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <span class="modal-title" id="titleLabel">Account Deletion Confirmation</span>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <form action="{{ route('myaccount') }}" method="post">
+                    @csrf
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <span>Are you sure? This action is <b>irreversible</b> for your privacy. </span><br><br>
+                            <span>All your data will be removed from our database, except the custID (randomized, can't be used to identify you) and the booking history as we need it for bookkeeping and legal reasons. </span><br><br>
+                            <label>Retype password to confirm deletion</label>
+                            <input type="password" class="form-control" name="delete-password" placeholder="Retype your password" required>
+                            <input type="text" class="form-control" name="custID" value="{{ Auth::user()->id }}" style="display: none;">
+                            <input type="text" class="form-control" name="custName" value="{{ Auth::user()->name }}" style="display: none;">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn btn-danger" name="delete-account">DELETE MY ACCOUNT</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- InfoBox information modal -->
+    <div class="modal fade" id="infoBox" @if(!session('info')) data-backdrop="static" @endif data-keyboard="false" tabindex="-1" aria-labelledby="infoBoxLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="infoBoxLabel">Info</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    @if(session('info')) {{ session('info') }} @endif
+                    @if(session('alert')) {{ session('alert') }} @endif
+                    @error('password') {{ $message }} @enderror
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">
+                        @if (session('info'))
+                            {{ 'Okay' }}
+                        @else
+                            {{ 'Understood' }}
+                        @endif
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+@endsection
+
+@section('bottom-js')
+@if(session('alert') || session('info'))
+<script>
+    $('#infoBox').modal()
+</script>
+@endif
+
+@error('password')
+<script>
+    $('#infoBox').modal()
+</script>
+@enderror
+@endsection
