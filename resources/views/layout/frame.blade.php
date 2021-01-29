788a556b54eb83e9c102e9ecdcb7839a8cbad0f2
Fang Wei Jie <fangweijie1327@e.newera.edu.my>
2021-01-29 12:11:48 +0800

    Added hide from print to avoid elements being printed

--- resources/views/layout/frame.blade.php
+++ resources/views/layout/frame.blade.php
@@ -44,6 +44,12 @@
         .row-space {
             padding: 15px 0 15px 0;
         }
+
+        @media print {
+            .hide-from-print {
+                display: none;
+            }
+        }
     </style>
     @yield('extra-css')
 </head>
@@ -51,15 +57,13 @@
 <body>
     <wrapper class="d-flex flex-column">
         <!-- navbar/header -->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top hide-from-print">
             <a class="navbar-brand" href=" @guest {{ '/' }} @endguest @auth {{ 'mybookings' }} @endauth">
-                <img src="{{ asset('images/logo.svg') }}" width="30" height="30" class="d-inline-block align-top" alt=""
-                    loading="lazy">
+                <img src="{{ asset('images/logo.svg') }}" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
                 X Badminton Court
             </a>
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler"
-                aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
@@ -113,49 +117,49 @@
         </main>
 
         <!-- footer -->
-        <footer class="footer mt-auto py-3 bg-dark">
+        <footer class="footer mt-auto py-3 bg-dark hide-from-print">
             <div class="container">
                 <div class="row">
                     <div class="col">
                         <div class="col">
                             <h4 class="white">Operation Hours</h3>
-                            <span class="white">Opened Daily from 08:00 till 20:00</span>
+                                <span class="white">Opened Daily from 08:00 till 20:00</span>
                         </div>
                     </div>
 
                     <div class="col">
                         <div class="col">
                             <h4 class="white">Address</h3>
-                            <span class="white">Blok B & C, Lot 5, Seksyen 10, Jalan Bukit, 43000 Kajang, Selangor.
-                            </span>
+                                <span class="white">Blok B & C, Lot 5, Seksyen 10, Jalan Bukit, 43000 Kajang, Selangor.
+                                </span>
                         </div>
                     </div>
 
                     <div class="col">
                         <div class="col">
                             <h4 class="white">Contact Us</h3>
-                            <a class="white" href="tel:+(60)3-87654321">
-                                <i class="bi bi-telephone-fill"></i>
-                                03-8765 4321
-                            </a>
-                            <br>
-                            <a class="white" href="mailto:badmintoncourt@email.my">
-                                <i class="bi bi-envelope-fill"></i>
-                                badmintoncourt@email.my
-                            </a>
+                                <a class="white" href="tel:+(60)3-87654321">
+                                    <i class="bi bi-telephone-fill"></i>
+                                    03-8765 4321
+                                </a>
+                                <br>
+                                <a class="white" href="mailto:badmintoncourt@email.my">
+                                    <i class="bi bi-envelope-fill"></i>
+                                    badmintoncourt@email.my
+                                </a>
                         </div>
                     </div>
 
                     <div class="col">
                         <div class="col">
                             <h4 class="white">Legal</h3>
-                            <a class="white" href="{{ ('tos') }}">
-                                Terms of Service
-                            </a>
-                            <br>
-                            <a class="white" href="{{ ('privacy') }}">
-                                Privacy Policy
-                            </a>
+                                <a class="white" href="{{ ('tos') }}">
+                                    Terms of Service
+                                </a>
+                                <br>
+                                <a class="white" href="{{ ('privacy') }}">
+                                    Privacy Policy
+                                </a>
                         </div>
                     </div>
                 </div>
