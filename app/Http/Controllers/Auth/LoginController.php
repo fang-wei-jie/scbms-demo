b0a62d66a982ac6308d837f37efc6827c5c20917
Fang Wei Jie <fangweijie1327@e.newera.edu.my>
2021-01-31 15:03:22 +0800

    Login controller enabled for admin

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -12,34 +12,54 @@ class LoginController extends Controller
     function __construct()
     {
 
-        $this -> middleware(['guest']);
-
+        $this->middleware(['guest']);
     }
 
-    function view () {
+    function view()
+    {
 
-        return view ('auth.login');
-
+        return view('auth.login');
     }
 
-    function auth (Request $request) {
+    function auth(Request $request)
+    {
 
-        $this -> validate($request, [
+        if (isset($_POST['login'])) {
 
-            'email' => 'required | email',
-            'password' => 'required'
+            $this->validate($request, [
 
-        ]);
+                'email' => 'required | email',
+                'password' => 'required'
 
-        // login verification
-        if (!Auth::attempt($request -> only('email', 'password'), $request -> remember)) {
+            ]);
 
-            return back() -> with('status', 'Invalid login details');
+            // login verification
+            if (!Auth::attempt($request->only('email', 'password'), $request->remember)) {
+
+                return back()->with('status', 'Invalid login details');
+            }
+
+            return redirect()->route('mybookings');
+
+        } else if (isset($_POST['admin-login'])) {
+
+            $this->validate($request, [
+
+                // 'adminLoginID' => 'required | exists:adminLoginID',
+                'adminLoginID' => 'required',
+                'password' => 'required'
+
+            ]);
+
+            // login verification
+            if (!Auth::guard('admin')->attempt($request->only('adminLoginID', 'password'), $request->remember)) {
+
+                return back()->with(['status' => 'Invalid login details', 'switchtab' => 'pills-admin-login-tab']);
+            }
+
+            return redirect()->route('admin-dash');
+
 
         }
-
-        return redirect() -> route('mybookings');
-
     }
-
 }
