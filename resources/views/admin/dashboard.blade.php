8cbc7daa3e218a26fd7ad981ff81224458b3f4c2
Fang Wei Jie <fangweijie1327@e.newera.edu.my>
2021-02-06 10:49:43 +0800

    Added sales overview on dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -35,6 +35,83 @@ Dashboard - Admin
                     </a>
                 </div>
             </div>
+
+            <br>
+
+            <div class="card bg-info">
+                <div class="card-body">
+                    <h1 class="card-title text-white">Sales Performance</h1>
+                    <!-- Earnings Monthly Card -->
+                    <div class="card py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col d-flex justify-content-between">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        This Month's Revenue
+                                    </div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                        RM @if($monthSales->monthSales != 0){{ $monthSales->monthSales }} @else {{ '0' }} @endif
+                                    </div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <br>
+
+                    <!-- Earnings Daily Card -->
+                    <div class="card py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col d-flex justify-content-between">
+                                    <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                                        Today's Revenue
+                                    </div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                            RM @if($todaySales->todaySales != 0){{ $todaySales->todaySales }} @else {{ '0' }} @endif
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <br>
+
+                    <!-- Today Bookings' Sale Card -->
+                    <div class="card py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col d-flex justify-content-between">
+                                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                        Today Bookings' Sale
+                                    </div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                        RM @if($todayBookingSales->todayBookingSales != 0){{ $todayBookingSales->todayBookingSales }} @else {{ '0' }} @endif
+                                    </div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-comments fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <br>
+
+                    <a href="{{ route('admin.sales') }}" class="btn btn-primary">
+                        <i class="bi bi-cash-stack"></i>
+                        Sales Report
+                    </a>
+                </div>
+            </div>
         </div>
 
         <div class="col">
