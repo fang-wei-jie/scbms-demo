659670c110559eef1a579655d8e42d9a0f9deebf
Fang Wei Jie <fangweijie1327@e.newera.edu.my>
2021-05-03 21:12:27 +0800

    Frame auto resize adjustments

--- resources/views/layout/frame.blade.php
+++ resources/views/layout/frame.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-100">
 
 <head>
     <meta charset="UTF-8">
@@ -24,29 +24,8 @@
 
     <!-- custom styles -->
     <style>
-        .row {
-            margin: 5px 0 5px 0;
-        }
-
-        main {
-            padding: 50px 50px 50px 50px;
-        }
-
-        body,
-        wrapper {
-            min-height: 100vh;
-        }
-
-        .flex-fill {
-            flex: 1 1 auto;
-        }
-
         .white {
             color: white;
-        }
-
-        .row-space {
-            padding: 15px 0 15px 0;
         }
 
         @media print {
@@ -62,160 +41,141 @@
     @yield('extra-css')
 </head>
 
-<body>
-    <wrapper class="d-flex flex-column">
-        <!-- navbar/header -->
-        <nav class="navbar navbar-expand-lg @if (request()->is('admin/*')) admin-red-bg navbar-dark @else bg-light navbar-light @endif fixed-top hide-from-print">
-            <a class="navbar-brand" href="
-                @guest {{ '/' }} @endguest
+<body class="d-flex flex-column h-100">
+
+    <!-- navbar/header -->
+    <nav id="header" class="navbar navbar-expand-lg @if (request()->is('admin/*')) admin-red-bg navbar-dark @else bg-light navbar-light @endif fixed-top hide-from-print">
+        <a class="navbar-brand" href="
+            @guest {{ '/' }} @endguest
+            @auth
+                @if (request()->is('admin/*'))
+                    {{ route('admin.dashboard') }}
+                @else
+                    {{ route('mybookings') }}
+                @endif
+            @endauth
+        ">
+
+            <img src="{{ asset('images/logo.svg') }}" width="30" height="30" class="d-inline-block align-top" @if (request()->is('admin/*')) style="filter: invert(100%) sepia(100%) saturate(0%) hue-rotate(115deg) brightness(108%) contrast(102%); " @endif alt="">
+            X Badminton Court
+        </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarToggler">
+            <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+
+                @guest
+
+                <li class="nav-item {{ (request()->is('about-us')) ? 'active font-weight-bold' : '' }}">
+                    <a class="nav-link" href="{{ ('about-us') }}">About Us</a>
+                </li>
+                <li class="nav-item {{ (request()->is('login')) ? 'active font-weight-bold' : '' }}">
+                    <a class="nav-link" href="{{ route('login') }}">Login</a>
+                </li>
+                <li class="nav-item {{ (request()->is('register')) ? 'active font-weight-bold' : '' }}">
+                    <a class="nav-link" href="{{ route('register') }}">Register</a>
+                </li>
+
+                @endguest
+
                 @auth
-                    @if (request()->is('admin/*'))
-                        {{ route('admin.dashboard') }}
-                    @else
-                        {{ route('mybookings') }}
-                    @endif
+
+                @if (request()->is('admin/*'))
+
+                    <li class="nav-item {{ (request()->is('admin/dashboard')) ? 'active font-weight-bold' : '' }}">
+                        <a class="nav-link" href="{{ route('admin.dashboard') }}">Dashboard</a>
+                    </li>
+                    <li class="nav-item {{ (request()->is('admin/checkin')) ? 'active font-weight-bold' : '' }}">
+                        <a class="nav-link" href="{{ route('admin.checkin') }}">Customer Check In</a>
+                    </li>
+                    <li class="nav-item {{ (request()->is('admin/bookings')) ? 'active font-weight-bold' : '' }}">
+                        <a class="nav-link" href="{{ route('admin.bookings') }}">Court Bookings</a>
+                    </li>
+                    <li class="nav-item {{ (request()->is('admin/rates')) ? 'active font-weight-bold' : '' }}">
+                        <a class="nav-link" href="{{ route('admin.rates') }}">Rates Management</a>
+                    </li>
+                    <li class="nav-item {{ (request()->is('admin/accounts')) ? 'active font-weight-bold' : '' }}">
+                        <a class="nav-link" href="{{ 'accounts' }}">Accounts Management</a>
+                    </li>
+                    <li class="nav-item {{ (request()->is('admin/sales')) ? 'active font-weight-bold' : '' }}">
+                        <a class="nav-link" href="{{ route('admin.sales') }}">Sales Report</a>
+                    </li>
+                    <li class="nav-item {{ (request()->is('admin/myaccount')) ? 'active font-weight-bold' : '' }}">
+                        <a class="nav-link" href="{{ route('admin.myadminaccount') }}">My Account</a>
+                    </li>
+
+                @else
+
+                    <li class="nav-item {{ (request()->is('book-court')) ? 'active font-weight-bold' : '' }}">
+                        <a class="nav-link" href="{{ route('book-court') }}">Book Courts</a>
+                    </li>
+                    <li class="nav-item {{ (request()->is('mybookings')) ? 'active font-weight-bold' : '' }}">
+                        <a class="nav-link" href="{{ route('mybookings') }}">My Bookings</a>
+                    </li>
+                    <li class="nav-item {{ (request()->is('myaccount')) ? 'active font-weight-bold' : '' }}">
+                        <a class="nav-link" href="{{ route('myaccount') }}">{{ session('custName') }}</a>
+                    </li>
+
+                @endif
+
+                <li class="nav-item">
+                    <a class="nav-link" style="color: red; " href="{{ ('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        Logout
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </li>
+
                 @endauth
-            ">
+            </ul>
+        </div>
+    </nav>
 
-                <img src="{{ asset('images/logo.svg') }}" width="30" height="30" class="d-inline-block align-top" @if (request()->is('admin/*')) style="filter: invert(100%) sepia(100%) saturate(0%) hue-rotate(115deg) brightness(108%) contrast(102%); " @endif alt="">
-                X Badminton Court
-            </a>
+    <div class="body @if(!request()->is('/')) mt-3 @endif">
+        @yield('body')
+    </div>
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarToggler">
-                <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-
-                    @guest
-
-                    <li class="nav-item {{ (request()->is('about-us')) ? 'active font-weight-bold' : '' }}">
-                        <a class="nav-link" href="{{ ('about-us') }}">About Us</a>
-                    </li>
-                    <li class="nav-item {{ (request()->is('login')) ? 'active font-weight-bold' : '' }}">
-                        <a class="nav-link" href="{{ route('login') }}">Login</a>
-                    </li>
-                    <li class="nav-item {{ (request()->is('register')) ? 'active font-weight-bold' : '' }}">
-                        <a class="nav-link" href="{{ route('register') }}">Register</a>
-                    </li>
-
-                    @endguest
-
-                    @auth
-
-                    @if (request()->is('admin/*'))
-
-                        <li class="nav-item {{ (request()->is('admin/dashboard')) ? 'active font-weight-bold' : '' }}">
-                            <a class="nav-link" href="{{ route('admin.dashboard') }}">Dashboard</a>
-                        </li>
-                        <li class="nav-item {{ (request()->is('admin/checkin')) ? 'active font-weight-bold' : '' }}">
-                            <a class="nav-link" href="{{ route('admin.checkin') }}">Customer Check In</a>
-                        </li>
-                        <li class="nav-item {{ (request()->is('admin/bookings')) ? 'active font-weight-bold' : '' }}">
-                            <a class="nav-link" href="{{ route('admin.bookings') }}">Court Bookings</a>
-                        </li>
-                        <li class="nav-item {{ (request()->is('admin/rates')) ? 'active font-weight-bold' : '' }}">
-                            <a class="nav-link" href="{{ route('admin.rates') }}">Rates Management</a>
-                        </li>
-                        <li class="nav-item {{ (request()->is('admin/accounts')) ? 'active font-weight-bold' : '' }}">
-                            <a class="nav-link" href="{{ 'accounts' }}">Accounts Management</a>
-                        </li>
-                        <li class="nav-item {{ (request()->is('admin/sales')) ? 'active font-weight-bold' : '' }}">
-                            <a class="nav-link" href="{{ route('admin.sales') }}">Sales Report</a>
-                        </li>
-                        <li class="nav-item {{ (request()->is('admin/myaccount')) ? 'active font-weight-bold' : '' }}">
-                            <a class="nav-link" href="{{ route('admin.myaccount') }}">My Account</a>
-                        </li>
-
-                    @else
-
-                        <li class="nav-item {{ (request()->is('book-court')) ? 'active font-weight-bold' : '' }}">
-                            <a class="nav-link" href="{{ route('book-court') }}">Book Courts</a>
-                        </li>
-                        <li class="nav-item {{ (request()->is('mybookings')) ? 'active font-weight-bold' : '' }}">
-                            <a class="nav-link" href="{{ route('mybookings') }}">My Bookings</a>
-                        </li>
-                        <li class="nav-item {{ (request()->is('myaccount')) ? 'active font-weight-bold' : '' }}">
-                            <a class="nav-link" href="{{ route('myaccount') }}">{{ session('custName') }}</a>
-                        </li>
-
-                    @endif
-
-                    <li class="nav-item">
-                        <a class="nav-link" style="color: red; " href="{{ ('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                            Logout
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
-                        </form>
-                    </li>
-
-                    @endauth
-                </ul>
+    <!-- footer -->
+    @if (!request()->is('admin/*'))
+    <footer class="footer mt-auto py-3 bg-dark hide-from-print">
+        <div class="container d-flex justify-content-between flex-wrap">
+            <div>
+                <span class="white">&#169; {{ date('Y') }} X Badminton Court Sdn Bhd</span>
             </div>
-        </nav>
-
-        <!-- body/content -->
-        <main class="flex-fill" style="padding-top: 75px;">
-            @yield('body')
-        </main>
-
-        @if (!request()->is('admin/*'))
-        <!-- footer -->
-        <footer class="footer mt-auto py-3 bg-dark hide-from-print">
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <div class="col">
-                            <h4 class="white">Operation Hours</h3>
-                                <span class="white">Opened Daily from 08:00 till 20:00</span>
-                        </div>
-                    </div>
-
-                    <div class="col">
-                        <div class="col">
-                            <h4 class="white">Address</h3>
-                                <span class="white">Blok B & C, Lot 5, Seksyen 10, Jalan Bukit, 43000 Kajang, Selangor.
-                                </span>
-                        </div>
-                    </div>
-
-                    <div class="col">
-                        <div class="col">
-                            <h4 class="white">Contact Us</h3>
-                                <a class="white" href="tel:+(60)3-87654321">
-                                    <i class="bi bi-telephone-fill"></i>
-                                    03-8765 4321
-                                </a>
-                                <br>
-                                <a class="white" href="mailto:badmintoncourt@email.my">
-                                    <i class="bi bi-envelope-fill"></i>
-                                    badmintoncourt@email.my
-                                </a>
-                        </div>
-                    </div>
-
-                    <div class="col">
-                        <div class="col">
-                            <h4 class="white">Legal</h3>
-                                <a class="white" href="{{ ('tos') }}">
-                                    Terms of Service
-                                </a>
-                                <br>
-                                <a class="white" href="{{ ('privacy') }}">
-                                    Privacy Policy
-                                </a>
-                        </div>
-                    </div>
-                </div>
+            <div>
+                <a class="white" href="{{ ('tos') }}">
+                    Terms of Service
+                </a>
+                <a class="white" href="{{ ('privacy') }}">
+                    Privacy Policy
+                </a>
             </div>
-        </footer>
-        @endif
-    </wrapper>
+        </div>
+    </footer>
+    @endif
 </body>
 
 @yield('bottom-js')
+
+<script>
+    // code below was used to dynamically resize the content padding to accomodate to the change of header height
+    resizeContentPadding()
+
+    $(window).on('resize', function(){
+        resizeContentPadding()
+    })
+
+    function resizeContentPadding() {
+        headerHeight = parseInt(document.getElementById('header').clientHeight)
+        console.log(headerHeight)
+        $('.body').css('padding-top', headerHeight)
+    }
+    // dynamic header resize feature end
+</script>
 
 </html>
