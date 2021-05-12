f0ab1f79c9b1fbf17a4c8d1408279a51cb44eb91
Fang Wei Jie <fangweijie1327@e.newera.edu.my>
2021-05-12 12:46:42 +0800

    Admin account is able to change name and password now

---
+++ app/Http/Controllers/Admin/AdminAccountController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AdminAccountController extends Controller
+{
+    function __construct()
+    {
+
+        $this -> middleware('auth:admin');
+
+    }
+
+    function view () {
+
+        return view ('admin.myadminaccount');
+
+    }
+
+    function update (Request $request) {
+
+        $admin = Auth::guard('admin') -> user();
+
+        if (isset ($_POST["change-name"]) ) {
+
+            // validation
+            $this -> validate($request, [
+
+                'name' => 'required',
+
+            ]);
+
+            // save changes
+            $admin->name = $request->input('name');
+            $admin->save();
+
+            // redirect back to page with info prompt
+            return back() -> with('info', 'Name updated');
+
+        } else if (isset ($_POST["change-password"]) ) {
+
+            // validate
+            $this -> validate($request, [
+
+                'old-password' => 'required',
+                'new-password' => 'required',
+                'confirm-password' => 'required',
+
+            ]);
+
+            // check if password matches, if not redirect back with alert prompt
+            if (!Hash::check($request->input('old-password'), $admin->password)) {
+
+                return back() -> with('alert', 'Current password incorrect. Password not updated. ');
+
+            } else {
+
+                // check if new password matches confirm password field, if not redirect back with alert propr
+                if ($request->input('new-password') != $request->input('confirm-password')) {
+
+                    return back() -> with('alert', 'New password and confirm password does not match. Password not updated. ');
+
+                }
+
+            }
+
+            // save changes
+            $admin->password = Hash::make($request->input('new-password'));
+            $admin->save();
+
+            // redirect back to page with info prompt
+            return back() -> with('info', 'Password updated. ');
+
+        }
+
+    }
+}
---
+++ resources/views/admin/myadminaccount.blade.php
@@ -0,0 +1,156 @@
+@extends('layout.frame')
+
+@section('title')
+My Admin Account -
+@endsection
+
+@section('body')
+<div class="container">
+    <h1>My Account</h1>
+
+    <hr>
+
+    <table class="table table-borderless" width="100vw">
+        <thead>
+            <tr>
+                <th width="45%"></th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    <div class="col">
+                        <h3>Name</h3>
+                        <span>{{ Auth::user()->name }}</span>
+                    </div>
+                </td>
+                <td>
+                    <button type="button" class="btn btn-outline-primary" id="changeNameButton" data-toggle="modal" data-target="#changeName">
+                        <i class="bi bi-pencil-fill"></i>
+                        Change
+                    </button>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <div class="col">
+                        <h3>Password</h3>
+                        <span>Change password every few months for security</span>
+                    </div>
+                </td>
+                <td>
+                    <button class="btn btn-outline-primary" id="change-password-button" data-toggle="modal" data-target="#changePassword">
+                        <i class="bi bi-pencil-fill"></i>
+                        Change
+                    </button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+    <!-- change name modal view -->
+    <div class="modal fade" id="changeName" tabindex="-1" role="dialog" aria-labelledby="changeNameLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <span class="modal-title" id="titleLabel">Change name</span>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <form action="" method="post">
+                    @csrf
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label>Enter your new name</label>
+                            <input type="text" class="form-control" name="name" maxlength="255" placeholder="Maximum 255 characters" required>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn btn-primary" name="change-name">Change name</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- change passsword modal view -->
+    <div class="modal fade" id="changePassword" tabindex="-1" role="dialog" aria-labelledby="changePasswordLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <span class="modal-title" id="titleLabel">Change Password</span>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <form action="" method="post">
+                    @csrf
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label>Enter current password</label>
+                            <input id="old-password" class="form-control" type="password" name="old-password" placeholder="Enter current password" required><br>
+                            <label>Enter new password</label>
+                            <input id="new-password" class="form-control" type="password" name="new-password" minlength=8 placeholder="Minimum of 8 characters" required><br>
+                            <label>Confirm new password</label>
+                            <input id="confirm-password" class="form-control" type="password" name="confirm-password" minlength=8 placeholder="Retype new password" required>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button class="btn btn-primary" type="submit" name="change-password">Change password</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- InfoBox information modal -->
+    <div class="modal fade" id="infoBox" @if(!session('info')) data-backdrop="static" @endif data-keyboard="false" tabindex="-1" aria-labelledby="infoBoxLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="infoBoxLabel">Info</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    @if(session('info')) {{ session('info') }} @endif
+                    @if(session('alert')) {{ session('alert') }} @endif
+                    @error('password') {{ $message }} @enderror
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">
+                        @if (session('info'))
+                            {{ 'Okay' }}
+                        @else
+                            {{ 'Understood' }}
+                        @endif
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+@endsection
+
+@section('bottom-js')
+@if(session('alert') || session('info'))
+<script>
+    $('#infoBox').modal()
+</script>
+@endif
+
+@error('password')
+<script>
+    $('#infoBox').modal()
+</script>
+@enderror
+@endsection
