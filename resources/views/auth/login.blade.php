205f65581436a9d0836a64ce8da1f93984bcb1d5
Fang Wei Jie <fangweijie1327@e.newera.edu.my>
2021-05-12 11:58:40 +0800

    Minor width adjustment to the login and register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,51 +6,45 @@ Login -
 
 @section('body')
 <div class="container">
+    <div class="col-sm">
+        <h1>Login</h1>
 
-    <div class="row justify-content-center">
+        <form method="post" action="{{ route('login') }}">
+            @csrf
 
-        <div class="col-sm">
-            <h1>Login</h1>
+            @if (session('status'))
+            <div class="alert alert-danger" role="alert">
+                {{ session('status') }}
+            </div>
+            @endif
 
-            <form method="post" action="{{ route('login') }}">
-                @csrf
-
-                @if (session('status'))
-                <div class="alert alert-danger" role="alert">
-                    {{ session('status') }}
+            <div class="form-group">
+                <input id="email" class="form-control @error('email') is-invalid @enderror @if(old('email')) is-valid @endif" type="text" name="email" value="{{ old('email') }}" maxlength=255 placeholder="Email address / Admin ID">
+                @error('email')
+                <div class="invalid-feedback">
+                    {{ $message }}
                 </div>
-                @endif
+                @enderror
+            </div>
 
-                <div class="form-group">
-                    <input id="email" class="form-control @error('email') is-invalid @enderror @if(old('email')) is-valid @endif" type="text" name="email" value="{{ old('email') }}" maxlength=255 placeholder="Email address / Admin ID">
-                    @error('email')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                    @enderror
+            <div class="form-group">
+                <input id="password" class="form-control @error('password') is-invalid @enderror" type="password" name="password" placeholder="Password">
+                @error('password')
+                <div class="invalid-feedback">
+                    {{ $message }}
                 </div>
+                @enderror
+            </div>
 
-                <div class="form-group">
-                    <input id="password" class="form-control @error('password') is-invalid @enderror" type="password" name="password" placeholder="Password">
-                    @error('password')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                    @enderror
-                </div>
+            <div class="form-group">
+                <button class="btn btn-primary" type="submit" name="login">Login as Customer</button>
+                <button class="btn btn-danger" type="submit" name="admin-login">Login as Admin</button>
+            </div>
 
-                <div class="form-group">
-                    <button class="btn btn-primary" type="submit" name="login">Login as Customer</button>
-                    <button class="btn btn-danger" type="submit" name="admin-login">Login as Admin</button>
-                </div>
-
-                <div class="form-group">
-                    Don't have an account? <a href="{{ route('register') }}"> Register now</a>
-                </div>
-            </form>
-        </div>
+            <div class="form-group">
+                Don't have an account? <a href="{{ route('register') }}"> Register now</a>
+            </div>
+        </form>
     </div>
-
-
 </div>
 @endsection
