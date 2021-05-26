342a5f7547ebf986845f34e7505d5fcf5a628a8d
Fang Wei Jie <fangweijie1327@e.newera.edu.my>
2021-05-26 18:50:55 +0800

    Fix logout failure

--- app/Http/Controllers/Auth/LogoutController.php
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class LogoutController extends Controller
@@ -11,8 +10,8 @@ class LogoutController extends Controller
     function logout () {
 
         Auth::logout();
-        Auth::guard('admin')->logout;
-        Auth::guard('manager')->logout;
+        Auth::guard('admin')->logout();
+        Auth::guard('manager')->logout();
 
         return redirect('/login');
 
