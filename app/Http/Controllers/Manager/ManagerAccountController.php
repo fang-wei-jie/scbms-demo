8e6ab3103ca5e5d3c87768e2f600ec01924385c4
Fang Wei Jie <fangweijie1327@e.newera.edu.my>
2021-05-26 18:54:38 +0800

    Manager's my account page completed

---
+++ app/Http/Controllers/Manager/ManagerAccountController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Manager;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class ManagerAccountController extends Controller
+{
+    function __construct()
+    {
+
+        $this -> middleware('auth:manager');
+
+    }
+
+    function view () {
+
+        return view ('manager.myaccount');
+
+    }
+
+    function update (Request $request) {
+
+        $manager = Auth::guard('manager') -> user();
+
+        if (isset ($_POST["change-name"]) ) {
+
+            // validation
+            $this -> validate($request, [
+
+                'name' => 'required',
+
+            ]);
+
+            // save changes
+            $manager->name = $request->input('name');
+            $manager->save();
+
+            // redirect back to page with info prompt
+            return back() -> with('info', 'Name updated');
+
+        } else if (isset ($_POST["change-password"]) ) {
+
+            // validate
+            $this -> validate($request, [
+
+                'old-password' => 'required',
+                'new-password' => 'required',
+                'confirm-password' => 'required',
+
+            ]);
+
+            // check if password matches, if not redirect back with alert prompt
+            if (!Hash::check($request->input('old-password'), $manager->password)) {
+
+                return back() -> with('alert', 'Current password incorrect. Password not updated. ');
+
+            } else {
+
+                // check if new password matches confirm password field, if not redirect back with alert propr
+                if ($request->input('new-password') != $request->input('confirm-password')) {
+
+                    return back() -> with('alert', 'New password and confirm password does not match. Password not updated. ');
+
+                }
+
+            }
+
+            // save changes
+            $manager->password = Hash::make($request->input('new-password'));
+            $manager->save();
+
+            // redirect back to page with info prompt
+            return back() -> with('info', 'Password updated. ');
+
+        }
+
+    }
+}
