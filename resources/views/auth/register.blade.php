1c7b90d062650596c9e7be9f8dcb99c429301967
Fang Wei Jie <fangweijie1327@e.newera.edu.my>
2021-06-02 15:56:05 +0800

    Register form now officially for registering users only

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,48 +8,6 @@ Register -
 <div class="container">
     <div class="col-sm">
         <h1>Register</h1>
-        {{-- <form method="post" action="{{ route('register') }}">
-        @csrf
-
-        <div class="form-group">
-            <h5>Register</h5>
-        </div>
-
-        <div class="form-group">
-            <input id="name" class="form-control @error('name') is-invalid @enderror @if(old('name')) is-valid @endif" type="text" name="name" value="{{ old('name') }}" maxlength=255 placeholder="Name" required>
-            @error('name')
-            <div class="invalid-feedback">
-                {{ $message }}
-            </div>
-            @enderror
-        </div>
-
-        <div class="form-group">
-            <input id="adminLoginID" class="form-control @error('adminLoginID') is-invalid @enderror @if(old('adminLoginID')) is-valid @endif" type="text" name="adminLoginID" value="{{ old('adminLoginID') }}" required>
-            @error('adminLoginID')
-            <div class="invalid-feedback">
-                {{ $message }}
-            </div>
-            @enderror
-        </div>
-
-        <div class="form-group">
-            <input id="password" class="form-control @error('password') is-invalid @enderror" type="password" name="password" value="{{ old('password') }}" minlength="8" placeholder="Password (Minimum 8 characters)" required>
-            @error('password')
-            <div class="invalid-feedback">
-                {{ $message }}
-            </div>
-            @enderror
-        </div>
-
-        <div class="form-group">
-            <input id="password_confirmation" class="form-control  @error('password') is-invalid @enderror" type="password" name="password_confirmation" value="{{ old('password') }}" minlength="8" placeholder="Retype password again" required>
-        </div>
-
-        <div class="form-group">
-            <button class="btn btn-primary" type="submit" name="register">Register</button>
-        </div>
-        </form> --}}
 
         <form method="post" action="{{ route('register') }}">
             @csrf
@@ -64,8 +22,8 @@ Register -
             </div>
 
             <div class="form-group">
-                <input id="adminLoginID" class="form-control @error('adminLoginID') is-invalid @enderror @if(old('adminLoginID')) is-valid @endif" type="tel" name="adminLoginID" value="{{ old('adminLoginID') }}" minlength="10" maxlength=11 placeholder="Mobile Phone Number (01XXXXXXXXX)" required>
-                @error('adminLoginID')
+                <input id="phone" class="form-control @error('phone') is-invalid @enderror @if(old('phone')) is-valid @endif" type="tel" name="phone" value="{{ old('phone') }}" minlength="10" maxlength=11 placeholder="Mobile Phone Number (01XXXXXXXXX)" required>
+                @error('phone')
                 <div class="invalid-feedback">
                     {{ $message }}
                 </div>
