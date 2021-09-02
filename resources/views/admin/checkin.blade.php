0873c3a1710860e2596cbd855b13797f1352c3c3
Wei Jie Jie <>
2021-09-02 21:01:01 +0800

    Check in now continuously search for QRcode

--- resources/views/admin/checkin.blade.php
+++ resources/views/admin/checkin.blade.php
@@ -58,20 +58,16 @@ Check In - Admin
     </div>
 
     <div class="row justify-content-md-center">
-        <div class="col-3">
+        <div class="col-auto">
             <form action="{{ route('admin.checkin') }}" method="post">
                 @csrf
-                <input id="resultToQuery" class="form-control" name="resultToQuery" type="text" placeholder="Check-in ID (14 digit code)" value="" minlength=14 maxlength="14" required>
+                <input id="resultToQuery" class="form-control" name="resultToQuery" type="text" placeholder="Scan or type check in code" value="" minlength=14 maxlength="14" required>
         </div>
-        <div class="col-1.5">
+        <div class="col-auto">
             <button class="btn btn-primary" type="submit" id="startQuery" name="startQuery">
                 <i class="bi bi-person-check-fill"></i>
                 Check-in
             </button>
-            <a href="{{ route('admin.checkin') }}" class="btn btn-primary">
-                <i class="bi bi-upc-scan"></i>
-                Scan Again
-            </a>
             </form>
         </div>
     </div>
