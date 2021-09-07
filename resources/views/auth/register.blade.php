f2ed6b2124047276ee6a7dcec859ff4f190451ec
Wei Jie Jie <>
2021-09-07 15:32:28 +0800

    Fix form no feedback to user

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,7 +10,7 @@ Register -
     <h1>Register</h1>
 
     <div class="form-group">
-        <input id="name" class="form-control @error('name') is-invalid @enderror @if(old('name')) is-valid @endif" type="text" name="name" value="{{ old('name') }}" maxlength=255 placeholder="Name" required>
+        <input id="name" class="form-control @error('name') is-invalid @enderror @if(old('name')) is-valid @endif" type="text" name="name" value="{{ old('name') }}" maxlength=255 placeholder="Name">
         @error('name')
         <div class="invalid-feedback">
             {{ $message }}
@@ -19,7 +19,7 @@ Register -
     </div>
 
     <div class="form-group">
-        <input id="phone" class="form-control @error('phone') is-invalid @enderror @if(old('phone')) is-valid @endif" type="tel" name="phone" value="{{ old('phone') }}" minlength="10" maxlength=11 placeholder="Mobile Phone Number (01XXXXXXXXX)" required>
+        <input id="phone" class="form-control @error('phone') is-invalid @enderror @if(old('phone')) is-valid @endif" type="tel" name="phone" value="{{ old('phone') }}" minlength="10" maxlength=11 placeholder="Mobile Phone Number (01XXXXXXXXX)">
         @error('phone')
         <div class="invalid-feedback">
             {{ $message }}
@@ -28,7 +28,7 @@ Register -
     </div>
 
     <div class="form-group">
-        <input id="email" class="form-control @error('email') is-invalid @enderror @if(old('email')) is-valid @endif" type="email" name="email" value="{{ old('email') }}" maxlength=75 placeholder="Email Address" required>
+        <input id="email" class="form-control @error('email') is-invalid @enderror @if(old('email')) is-valid @endif" type="email" name="email" value="{{ old('email') }}" maxlength=75 placeholder="Email Address">
         @error('email')
         <div class="invalid-feedback">
             {{ $message }}
@@ -37,7 +37,7 @@ Register -
     </div>
 
     <div class="form-group">
-        <input id="password" class="form-control @error('password') is-invalid @enderror" type="password" name="password" value="{{ old('password') }}" minlength="8" placeholder="Password (Minimum 8 characters)" required>
+        <input id="password" class="form-control @error('password') is-invalid @enderror" type="password" name="password" value="{{ old('password') }}" minlength="8" placeholder="Password (Minimum 8 characters)">
         @error('password')
         <div class="invalid-feedback">
             {{ $message }}
@@ -46,11 +46,16 @@ Register -
     </div>
 
     <div class="form-group">
-        <input id="password_confirmation" class="form-control  @error('password') is-invalid @enderror" type="password" name="password_confirmation" value="{{ old('password') }}" minlength="8" placeholder="Retype password again" required>
+        <input id="password_confirmation" class="form-control  @error('password') is-invalid @enderror" type="password" name="password_confirmation" value="{{ old('password') }}" minlength="8" placeholder="Retype password again">
+        @error('password')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+        @enderror
     </div>
 
     <div class="form-group">
-        <button class="btn btn-primary" type="submit" name="register">Register</button>
+        <button class="btn btn-primary btn-block" type="submit" name="register">Register</button>
     </div>
 
     <div class="form-group">
