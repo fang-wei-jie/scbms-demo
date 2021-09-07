895608fa4c1f3ea8883824ddbfe316b19648dee7
Wei Jie Jie <>
2021-09-07 14:51:06 +0800

    Updated forms width representation

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,52 +5,37 @@ Login -
 @endsection
 
 @section('body')
-<div class="container">
-    <div class="col-sm">
-        <h1>Login</h1>
+<form class="form-signin" method="post" action="{{ route('login') }}">
+    @csrf
+    <h1>Login</h1>
 
-        <form method="post" action="{{ route('login') }}">
-            @csrf
-
-            @if (session('status'))
-            <div class="alert alert-danger" role="alert">
-                {{ session('status') }}
-            </div>
-            @endif
-
-            <div class="form-group">
-                <input id="email" class="form-control @error('email') is-invalid @enderror @if(old('email')) is-valid @endif" type="email" name="email" value="{{ old('email') }}" maxlength=255 placeholder="Email address">
-                @error('email')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
-                @enderror
-            </div>
-
-            <div class="form-group">
-                <input id="password" class="form-control @error('password') is-invalid @enderror" type="password" name="password" placeholder="Password">
-                @error('password')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
-                @enderror
-            </div>
-
-            <div class="form-group">
-                <button class="btn btn-primary" type="submit" name="login">
-                    <i class="bi bi-person-circle"></i>
-                    Login as Customer
-                </button>
-                <button class="btn btn-danger" type="submit" name="admin-login">
-                    <i class="bi bi-person-badge"></i>
-                    Login as Admin
-                </button>
-            </div>
-
-            <div class="form-group">
-                Don't have an account? <a href="{{ route('register') }}"> Register now</a>
-            </div>
-        </form>
+    @if (session('status'))
+    <div class="alert alert-danger" role="alert">
+        {{ session('status') }}
     </div>
-</div>
+    @endif
+
+    <div class="form-group">
+        <input id="email" class="form-control @error('email') is-invalid @enderror @if(old('email')) is-valid @endif" type="email" name="email" value="{{ old('email') }}" maxlength=255 placeholder="Email address">
+        @error('email')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+        @enderror
+    </div>
+
+    <div class="form-group">
+        <input id="password" class="form-control @error('password') is-invalid @enderror" type="password" name="password" placeholder="Password">
+        @error('password')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+        @enderror
+    </div>
+
+
+    <div class="form-group">
+        Don't have an account? <a href="{{ route('register') }}"> Register now</a>
+    </div>
+</form>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,61 +5,56 @@ Register -
 @endsection
 
 @section('body')
-<div class="container">
-    <div class="col-sm">
-        <h1>Register</h1>
+<form class="form-signin" method="post" action="{{ route('register') }}">
+    @csrf
+    <h1>Register</h1>
 
-        <form method="post" action="{{ route('register') }}">
-            @csrf
-
-            <div class="form-group">
-                <input id="name" class="form-control @error('name') is-invalid @enderror @if(old('name')) is-valid @endif" type="text" name="name" value="{{ old('name') }}" maxlength=255 placeholder="Name" required>
-                @error('name')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
-                @enderror
-            </div>
-
-            <div class="form-group">
-                <input id="phone" class="form-control @error('phone') is-invalid @enderror @if(old('phone')) is-valid @endif" type="tel" name="phone" value="{{ old('phone') }}" minlength="10" maxlength=11 placeholder="Mobile Phone Number (01XXXXXXXXX)" required>
-                @error('phone')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
-                @enderror
-            </div>
-
-            <div class="form-group">
-                <input id="email" class="form-control @error('email') is-invalid @enderror @if(old('email')) is-valid @endif" type="email" name="email" value="{{ old('email') }}" maxlength=75 placeholder="Email Address" required>
-                @error('email')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
-                @enderror
-            </div>
-
-            <div class="form-group">
-                <input id="password" class="form-control @error('password') is-invalid @enderror" type="password" name="password" value="{{ old('password') }}" minlength="8" placeholder="Password (Minimum 8 characters)" required>
-                @error('password')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
-                @enderror
-            </div>
-
-            <div class="form-group">
-                <input id="password_confirmation" class="form-control  @error('password') is-invalid @enderror" type="password" name="password_confirmation" value="{{ old('password') }}" minlength="8" placeholder="Retype password again" required>
-            </div>
-
-            <div class="form-group">
-                <button class="btn btn-primary" type="submit" name="register">Register</button>
-            </div>
-
-            <div class="form-group">
-                Already have an account? <a href="{{ route('login') }}">Login now</a>
-            </div>
-        </form>
+    <div class="form-group">
+        <input id="name" class="form-control @error('name') is-invalid @enderror @if(old('name')) is-valid @endif" type="text" name="name" value="{{ old('name') }}" maxlength=255 placeholder="Name" required>
+        @error('name')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+        @enderror
     </div>
-</div>
+
+    <div class="form-group">
+        <input id="phone" class="form-control @error('phone') is-invalid @enderror @if(old('phone')) is-valid @endif" type="tel" name="phone" value="{{ old('phone') }}" minlength="10" maxlength=11 placeholder="Mobile Phone Number (01XXXXXXXXX)" required>
+        @error('phone')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+        @enderror
+    </div>
+
+    <div class="form-group">
+        <input id="email" class="form-control @error('email') is-invalid @enderror @if(old('email')) is-valid @endif" type="email" name="email" value="{{ old('email') }}" maxlength=75 placeholder="Email Address" required>
+        @error('email')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+        @enderror
+    </div>
+
+    <div class="form-group">
+        <input id="password" class="form-control @error('password') is-invalid @enderror" type="password" name="password" value="{{ old('password') }}" minlength="8" placeholder="Password (Minimum 8 characters)" required>
+        @error('password')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+        @enderror
+    </div>
+
+    <div class="form-group">
+        <input id="password_confirmation" class="form-control  @error('password') is-invalid @enderror" type="password" name="password_confirmation" value="{{ old('password') }}" minlength="8" placeholder="Retype password again" required>
+    </div>
+
+    <div class="form-group">
+        <button class="btn btn-primary" type="submit" name="register">Register</button>
+    </div>
+
+    <div class="form-group">
+        Already have an account? <a href="{{ route('login') }}">Login now</a>
+    </div>
+</form>
 @endsection
