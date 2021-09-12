ee7b83a18005892a23e3f8efbb0bc62a234e10a6
Wei Jie Jie <>
2021-09-12 21:48:49 +0800

    Sales Report page quick statistics slimmer and more useful
    Now displays sales of the day, week, month, and year

--- app/Http/Controllers/Manager/SalesController.php
+++ app/Http/Controllers/Manager/SalesController.php
@@ -15,10 +15,24 @@ class SalesController extends Controller
     function view()
     {
 
-        // SELECT SUM(timeLength*bookingPrice) as monthSales FROM bookings WHERE created_at LIKE '2020-12%'
+        $yearSales = DB::table('bookings')
+            ->selectRaw('SUM(timeLength*bookingPrice) as yearSales')
+            ->where('created_at', 'LIKE', date('Y-').'%')
+            ->first();
+
         $monthSales = DB::table('bookings')
             ->selectRaw('SUM(timeLength*bookingPrice) as monthSales')
             ->where('created_at', 'LIKE', date('Y-m').'%')
+            ->first();
+
+        $dayOfWeek = date("w");
+        $startDateNumber = str_pad(date("d") - $dayOfWeek, 2, "0", STR_PAD_LEFT);
+        $endDateNumber = str_pad(date("d") + (6 - $dayOfWeek), 2, "0", STR_PAD_LEFT);
+        $weekQuery = '`created_at` BETWEEN "'.date('Y-m-').$startDateNumber.'%" AND "'.date('Y-m-').$endDateNumber.'%"';
+
+        $weekSales = DB::table('bookings')
+            ->selectRaw('SUM(timeLength*bookingPrice) as weekSales')
+            ->whereRaw($weekQuery)
             ->first();
 
         $todaySales = DB::table('bookings')
@@ -26,11 +40,18 @@ class SalesController extends Controller
             ->where('created_at', 'LIKE', date('Y-m-d').'%')
             ->first();
 
-        $todayBookingSales = DB::table('bookings')
-            ->selectRaw('SUM(timeLength*bookingPrice) as todayBookingSales')
-            ->where('dateSlot', '=', date('Ymd'))
-            ->first();
+        $todayBookings = DB::table('bookings')
+            ->join('rates', 'rates.id', '=', 'bookings.rateID')
+            ->join('users', 'users.id', '=', 'bookings.custID')
+            ->whereRaw('`bookings`.`created_at` LIKE "'.date("Y-m-d").'%"')
+            ->orderByDesc('bookings.created_at')
+            ->get();
 
-        return view('manager.sales', ['monthSales' => $monthSales, 'todaySales' => $todaySales, 'todayBookingSales' => $todayBookingSales]);
+        return view('manager.sales', [
+            'yearSales' => $yearSales->yearSales,
+            'monthSales' => $monthSales->monthSales,
+            'weekSales' => $weekSales->weekSales,
+            'todaySales' => $todaySales->todaySales,
+        ]);
     }
 }
--- resources/views/manager/sales.blade.php
+++ resources/views/manager/sales.blade.php
@@ -5,26 +5,88 @@ Sales Report - Manager
 @endsection
 
 @section('body')
-<div class="container-fluid">
-    <div class="card-deck justify-content-md-center">
-        <!-- Earnings Monthly Card -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col d-flex justify-content-between">
-                            <div class="text-xs font-weight-bold text-primary mb-1">
-                                This Month's Revenue
+    <div class="container">
+
+        <div>
+            <div class="row">
+                <div class="col">
+                    <div class="card py-2">
+                        <div class="mx-3 my-1">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col d-flex justify-content-between">
+                                    <div class="text-xs font-weight-bold text-warning mb-1">
+                                        Today
+                                    </div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                        RM @if ($todaySales != 0){{ $todaySales }} @else {{ '0' }} @endif
+                                    </div>
+                                </div>
                             </div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                RM @if($monthSales->monthSales != 0){{ $monthSales->monthSales }} @else {{ '0' }} @endif
-                            </div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
                         </div>
                     </div>
                 </div>
+
+                <br>
+
+                <div class="col">
+                    <div class="card py-2">
+                        <div class="mx-3 my-1">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col d-flex justify-content-between">
+                                    <div class="text-xs font-weight-bold text-info mb-1">
+                                        This Week
+                                    </div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                            RM @if ($weekSales != 0){{ $weekSales }} @else {{ '0' }} @endif
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <br>
+
+                <div class="col">
+                    <div class="card py-2">
+                        <div class="mx-3 my-1">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col d-flex justify-content-between">
+                                    <div class="text-xs font-weight-bold text-primary mb-1">
+                                        This Month
+                                    </div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                        RM @if ($monthSales != 0){{ $monthSales }} @else {{ '0' }} @endif
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <br>
+
+                <div class="col">
+                    <div class="card py-2">
+                        <div class="mx-3 my-1">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col d-flex justify-content-between">
+                                    <div class="text-xs font-weight-bold text-primary mb-1">
+                                        This Year
+                                    </div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                        RM @if ($yearSales != 0){{ $yearSales }} @else {{ '0' }} @endif
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <br>
+
             </div>
         </div>
 
