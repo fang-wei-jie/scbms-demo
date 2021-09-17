b9abc007349a999a74b32a4b5ae5ec1239a0f707
Wei Jie Jie <>
2021-09-17 23:08:00 +0800

    BS5ed landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,33 +6,39 @@
         font-size: 100px;
     }
 
+    .carousel-image {
+        height: 50vh;
+        object-fit: cover;
+        object-position: center;
+    }
+</style>
 @endsection
 
 @section('body')
-<div id="carouselIndicators" class="carousel slide" data-ride="carousel">
+<div id="carouselIndicators" class="carousel slide" data-bs-ride="carousel">
     <ol class="carousel-indicators">
-        <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselIndicators" data-slide-to="2"></li>
+        <li data-bs-target="#carouselIndicators" data-bs-slide-to="0" class="active"></li>
+        <li data-bs-target="#carouselIndicators" data-bs-slide-to="1"></li>
+        <li data-bs-target="#carouselIndicators" data-bs-slide-to="2"></li>
     </ol>
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img src="https://generative-placeholders.glitch.me/image?width=2000&height=600&img=01" class="d-block w-100" alt="...">
+            <img src="{{ URL::asset('images/banner/banner-1.jpg') }}" class="d-block w-100 carousel-image" alt="...">
         </div>
         <div class="carousel-item">
-            <img src="https://generative-placeholders.glitch.me/image?width=2000&height=600&img=02" class="d-block w-100" alt="...">
+            <img src="{{ URL::asset('images/banner/banner-2.jpg') }}" class="d-block w-100 carousel-image" alt="...">
         </div>
         <div class="carousel-item">
-            <img src="https://generative-placeholders.glitch.me/image?width=2000&height=600&img=03" class="d-block w-100" alt="...">
+            <img src="{{ URL::asset('images/banner/banner-3.jpg') }}" class="d-block w-100 carousel-image" alt="...">
         </div>
     </div>
-    <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
+    <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
+        <span class="visually-hidden">Previous</span>
     </a>
-    <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
+    <a class="carousel-control-next" href="#carouselIndicators" role="button" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
+        <span class="visually-hidden">Next</span>
     </a>
 </div>
 
