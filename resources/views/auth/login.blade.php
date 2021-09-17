bb9684b018ef43c7c81a1a0d47bbe45fe21f3110
Wei Jie Jie <>
2021-09-17 21:00:22 +0800

    BS5ed Login and register with floating labels

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,40 +7,33 @@ Login -
 @section('body')
 <form class="form-signin" method="post" action="{{ route('login') }}">
     @csrf
-    <h3>Login</h3>
+    <h3 class="mb-3">Login</h3>
 
     @if (session('status'))
     <div class="alert alert-danger" role="alert">
+        <i class="bi bi-exclamation-lg"></i>
         {{ session('status') }}
     </div>
     @endif
 
-    <div class="form-group">
-        <input id="email" class="form-control @error('email') is-invalid @enderror @if(old('email')) is-valid @endif" type="email" name="email" value="{{ old('email') }}" maxlength=255 placeholder="Email address">
-        @error('email')
-        <div class="invalid-feedback">
-            {{ $message }}
-        </div>
-        @enderror
+    <div class="form-floating mb-3">
+        <input id="email" class="form-control @error('email') is-invalid @enderror @if(old('email')) is-valid @endif" type="text" name="email" value="{{ old('email') }}" maxlength=255 placeholder="Email address">
+        <label for="email">Email @error('email')({{ $message }})@enderror</label>
     </div>
 
-    <div class="form-group">
+    <div class="form-floating mb-3">
         <input id="password" class="form-control @error('password') is-invalid @enderror" type="password" name="password" placeholder="Password">
-        @error('password')
-        <div class="invalid-feedback">
-            {{ $message }}
-        </div>
-        @enderror
+        <label for="password">Password @error('password')({{ $message }})@enderror</label>
     </div>
 
-    <div class="form-group">
-        <button class="btn btn-primary btn-block" type="submit" name="login">
+    <div class="d-grid gap-2 mb-3">
+        <button class="btn btn-primary" type="submit" name="login">
             Login
         </button>
     </div>
 
 
-    <div class="form-group">
+    <div class="form-floating mb-3">
         Don't have an account? <a href="{{ route('register') }}"> Register now</a>
     </div>
 </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,58 +7,38 @@ Register -
 @section('body')
 <form class="form-signin" method="post" action="{{ route('register') }}">
     @csrf
-    <h3>Register</h3>
+    <h3 class="mb-3">Register</h3>
 
-    <div class="form-group">
+    <div class="form-floating mb-3">
         <input id="name" class="form-control @error('name') is-invalid @enderror @if(old('name')) is-valid @endif" type="text" name="name" value="{{ old('name') }}" maxlength=255 placeholder="Name">
-        @error('name')
-        <div class="invalid-feedback">
-            {{ $message }}
-        </div>
-        @enderror
+        <label for="name">Name @error('name')(Required)@enderror</label>
     </div>
 
-    <div class="form-group">
+    <div class="form-floating mb-3">
         <input id="phone" class="form-control @error('phone') is-invalid @enderror @if(old('phone')) is-valid @endif" type="tel" name="phone" value="{{ old('phone') }}" minlength="10" maxlength=11 placeholder="Mobile Phone Number (01XXXXXXXXX)">
-        @error('phone')
-        <div class="invalid-feedback">
-            {{ $message }}
-        </div>
-        @enderror
+        <label for="phone">Phone @error('phone')({{ $message }})@enderror</label>
     </div>
 
-    <div class="form-group">
-        <input id="email" class="form-control @error('email') is-invalid @enderror @if(old('email')) is-valid @endif" type="email" name="email" value="{{ old('email') }}" maxlength=75 placeholder="Email Address">
-        @error('email')
-        <div class="invalid-feedback">
-            {{ $message }}
-        </div>
-        @enderror
+    <div class="form-floating mb-3">
+        <input id="email" class="form-control @error('email') is-invalid @enderror @if(old('email')) is-valid @endif" type="text" name="email" value="{{ old('email') }}" maxlength=75 placeholder="Email Address">
+        <label for="email">Email @error('email')({{ $message }})@enderror</label>
     </div>
 
-    <div class="form-group">
+    <div class="form-floating mb-3">
         <input id="password" class="form-control @error('password') is-invalid @enderror" type="password" name="password" value="{{ old('password') }}" minlength="8" placeholder="Password (Minimum 8 characters)">
-        @error('password')
-        <div class="invalid-feedback">
-            {{ $message }}
-        </div>
-        @enderror
+        <label for="password">Password @error('password')({{ $message }})@enderror</label>
     </div>
 
-    <div class="form-group">
+    <div class="form-floating mb-3">
         <input id="password_confirmation" class="form-control  @error('password') is-invalid @enderror" type="password" name="password_confirmation" value="{{ old('password') }}" minlength="8" placeholder="Retype password again">
-        @error('password')
-        <div class="invalid-feedback">
-            {{ $message }}
-        </div>
-        @enderror
+        <label for="password_confirmation">Confirm Password @error('password_confirmation')({{ $message }})@enderror</label>
     </div>
 
-    <div class="form-group">
-        <button class="btn btn-primary btn-block" type="submit" name="register">Register</button>
+    <div class="d-grid gap-2 mb-3">
+        <button class="btn btn-primary" type="submit" name="register">Register</button>
     </div>
 
-    <div class="form-group">
+    <div class="form-floating mb-3">
         Already have an account? <a href="{{ route('login') }}">Login now</a>
     </div>
 </form>
