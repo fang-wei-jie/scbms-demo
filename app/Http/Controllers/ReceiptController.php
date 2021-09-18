920867ab8b93764d7d27127b5c29407580a1c1e9
Wei Jie Jie <>
2021-09-18 22:53:48 +0800

    Hugely simplified receipt page structure and mobile viewable

--- app/Http/Controllers/ReceiptController.php
+++ app/Http/Controllers/ReceiptController.php
@@ -24,6 +24,17 @@ class ReceiptController extends Controller
 
         ]);
 
+        $companyName = DB::table('operation_preferences')
+            ->where('attr', 'name')
+            ->first();
+
+        $logo = DB::table('ui_preferences')
+            ->where('side', '')
+            ->first()
+            ->logo;
+
+        if ($logo == null) { $logo = "https://icons.getbootstrap.com/assets/icons/hexagon-half.svg"; }
+
         $invoiceDetail = DB::table('bookings')
             -> join('users', 'bookings.custID', '=', 'users.id')
             -> join('rates', 'bookings.rateID', '=', 'rates.id')
@@ -31,7 +42,21 @@ class ReceiptController extends Controller
             -> select('bookingID', 'custID', 'bookings.created_at as bookingDateTime', 'name', 'phone', 'email', 'dateSlot', 'timeSlot', 'timeLength', 'courtID', 'bookingPrice', 'rateName')
             -> first();
 
-        return view('customer.invoice', ['invoiceDetail' => $invoiceDetail]);
+        return view('customer.invoice', [
+            'invoiceDetail' => $invoiceDetail,
+            'bookID' => str_pad($invoiceDetail->bookingID, 7, 0, STR_PAD_LEFT).str_pad($invoiceDetail->custID, 7, 0, STR_PAD_LEFT),
+            'createdOn' => $invoiceDetail->bookingDateTime,
+            'custName' => $invoiceDetail->name,
+            'custPhone' => $invoiceDetail->phone,
+            'custEmail' => $invoiceDetail->email,
+            'bookingDateTimeSlot' => substr($invoiceDetail->dateSlot, 6, 2) . "/" . substr($invoiceDetail->dateSlot, 4, 2) . "/" . substr($invoiceDetail->dateSlot, 0, 4) . " " . $invoiceDetail->timeSlot . ":00 - " . ($invoiceDetail->timeSlot + $invoiceDetail->timeLength) . ":00",
+            'courtID' => $invoiceDetail->courtID,
+            'rateName' => $invoiceDetail->rateName,
+            'companyName' => $companyName->value,
+            'ratePrice' => $invoiceDetail->bookingPrice,
+            'timeLength' => $invoiceDetail->timeLength,`
+            'logo' => $logo,
+        ]);
 
     }
 
--- resources/views/customer/invoice.blade.php
+++ resources/views/customer/invoice.blade.php
@@ -5,96 +5,120 @@ Receipt -
 @endsection
 
 @section('body')
-<h1>Receipt</h1>
+<div class="container">
 
-<div>
-    <table class="table table-borderless">
-        <tbody>
-            <tr>
-                <td width=50px>
-                    <img src="{{ asset('images/logo.svg') }}" height="50px">
-                </td>
-                <td>
-                    <h2>X Badminton Court</h2>
-                    <span>Blok B & C, Lot 5, Seksyen 10, Jalan Bukit, 43000 Kajang, Selangor.</span><br>
-                    <span>Tel: 03-8765 4321</span><br>
-                    <span>Email: badmintoncourt@email.my</span>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
-<div>
-    <table class="table table-borderless table-sm">
-        <tbody>
-            <tr>
-                <td colspan="2">
-                    <h4>Booking Details</h4>
-                    <hr>
-                    Invoice ID / Book ID: {{ str_pad($invoiceDetail->bookingID, 7, 0, STR_PAD_LEFT) }}{{ str_pad($invoiceDetail->custID, 7, 0, STR_PAD_LEFT) }}<br>
-                    Order Created On: {{ $invoiceDetail->bookingDateTime }}
-                </td>
+    <h1>Receipt</h1>
 
-                <td colspan="2">
-                    <h4>Customer Details</h4>
-                    <hr>
-                    Name: {{ $invoiceDetail->name }}<br>
-                    Phone: {{ $invoiceDetail->phone }}<br>
-                    Email: {{ $invoiceDetail->email }}
-                </td>
-            </tr>
+    <div class="my-2"></div>
 
-            <tr>
-                <td colspan="4">
-                    <h4>Purchase invoiceDetail</h4>
-                    <hr>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    {{ substr($invoiceDetail->dateSlot, 6, 2) }}/{{ substr($invoiceDetail->dateSlot, 4, 2) }}/{{ substr($invoiceDetail->dateSlot, 0, 4) }} {{ $invoiceDetail->timeSlot }}:00 - {{ ($invoiceDetail->timeSlot + $invoiceDetail->timeLength) }}:00<br>
-                    Court {{ $invoiceDetail->courtID }} {{ $invoiceDetail->rateName }}<br>
-                </td>
-                <td>
-                    RM{{ $invoiceDetail->bookingPrice }}/hour
-                </td>
-                <td>
-                    {{ $invoiceDetail->timeLength }} @if($invoiceDetail->timeLength > 1) hours @else hour @endif
-                </td>
-                <td>
-                    RM{{ ($invoiceDetail->bookingPrice * $invoiceDetail->timeLength) }}
-                </td>
-            </tr>
-            <tr>
-                <td colspan="3" class="text-right">
-                    Net total
-                </td>
-                <td>
-                    RM{{ round(($invoiceDetail->bookingPrice * $invoiceDetail->timeLength) / 106 * 100, 2) }}
-                </td>
-            </tr>
-            <tr>
-                <td colspan="3" class="text-right">
-                    SST (6%)
-                </td>
-                <td>
-                    RM{{ round(($invoiceDetail->bookingPrice * $invoiceDetail->timeLength) / 106 * 6, 2) }}
-                </td>
-            </tr>
-            <tr>
-                <td colspan="3" class="text-right">
-                    Total
-                </td>
-                <td>
-                    RM{{ ($invoiceDetail->bookingPrice * $invoiceDetail->timeLength) }}.00
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
-<div class="hide-from-print" style="display: flex; justify-content: center; padding-top: 30px;">
-    <button id="printPageButton" onclick="window.print();" class="btn btn-outline-primary hide-from-print">
-        Print
-    </button>
+    <div class="row">
+        <div class="col-xl">
+            <div class="my-2"></div>
+            <div class="card border border-#dfdfdf">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <span style="display: flex; justify-content: flex-start; align-items: center;">
+                                <img src="{{ $logo }}" height="50px">
+                                <h2>&nbsp;{{ $companyName }}</h2>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <span>Blok B & C, Lot 5, Seksyen 10, Jalan Bukit, 43000 Kajang, Selangor.</span><br>
+            <span>Tel: 03-8765 4321</span><br>
+            <span>Email: badmintoncourt@email.my</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="my-2"></div>
+            <div class="card border border-#dfdfdf">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <h5 class="card-title">Order Details</h5>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            Invoice / Book ID: {{ $bookID }}<br>
+                            Created On: {{ $createdOn }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="my-2"></div>
+            <div class="card border border-#dfdfdf">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <h5 class="card-title">Customer Details</h5>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            Name: {{ $custName }}<br>
+                            Phone: {{ $custPhone }}<br>
+                            Email: {{ $custEmail }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="my-2"></div>
+
+    <div class="card border border-#dfdfdf">
+        <div class="card-body">
+            <div class="row">
+                <div class="col">
+                    <h5 class="card-title">Purchase Detail</h5>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <table class="table ">
+                        <tr>
+                            <td>
+                                {{ $bookingDateTimeSlot }}<br>
+                                Court {{ $courtID }} {{ $rateName }}<br>
+                            </td>
+                            <td>
+                                RM{{ $ratePrice }}/hour
+                            </td>
+                            <td>
+                                {{ $timeLength }} @if($timeLength > 1) hours @else hour @endif
+                            </td>
+                            <td>
+                                RM{{ ($ratePrice * $timeLength) }}.00
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="3" class="text-right">
+                                <b>Total</b>
+                            </td>
+                            <td>
+                                <b>RM{{ ($ratePrice  * $timeLength) }}.00</b>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="hide-from-print" style="display: flex; justify-content: center; padding-top: 30px;">
+        <button id="printPageButton" onclick="window.print();" class="btn btn-outline-primary hide-from-print">
+            Print
+        </button>
+    </div>
 </div>
 @endsection
