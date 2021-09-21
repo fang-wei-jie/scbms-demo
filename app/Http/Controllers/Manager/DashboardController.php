8982e3ba45e1059fa34436b543b961a694eaadee
Wei Jie Jie <>
2021-09-21 23:23:34 +0800

    Remove preferences summary card from manager dashboard

--- app/Http/Controllers/Manager/DashboardController.php
+++ app/Http/Controllers/Manager/DashboardController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Manager;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -17,17 +16,7 @@ class DashboardController extends Controller
     function view ()
     {
 
-        $name = DB::table('operation_preferences')->where('attr', 'name')->first();
-        $domain = DB::table('operation_preferences')->where('attr', 'domain')->first();
-        $start_time = DB::table('operation_preferences')->where('attr', 'start_time')->first();
-        $end_time = DB::table('operation_preferences')->where('attr', 'end_time')->first();
-
-        return view ('manager.dashboard', [
-            "name" => $name->value,
-            "domain" => $domain->value,
-            "start_time" => $start_time->value,
-            "end_time" => $end_time->value,
-        ]);
+        return view ('manager.dashboard');
 
     }
 }
