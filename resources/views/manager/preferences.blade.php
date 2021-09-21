d3f23f5a02da069dcb2f4af097d495034cef0613
Wei Jie Jie <>
2021-09-22 00:10:38 +0800

    disabled label check now includes out of range values

--- resources/views/manager/preferences.blade.php
+++ resources/views/manager/preferences.blade.php
@@ -80,18 +80,18 @@
                         <small>Enable the deletion/cancelation of bookings made</small>
                     </div>
 
-                    <div class="form-check form-switch" id="bookingDeletableCustomer" @if($deleteBooking == 0) data-bs-toggle="tooltip" data-bs-placement="left" title="Enable Delete Booking to change this setting" @endif>
-                        <input class="form-check-input" type="checkbox" id="customerDeleteBooking" name="customerDeleteBooking" @if($customerDeleteBooking == 1){{ "checked" }}@endif @if($deleteBooking == 0){{ "disabled" }}@endif>
+                    <div class="form-check form-switch" id="bookingDeletableCustomer" @if($deleteBooking != 1) data-bs-toggle="tooltip" data-bs-placement="left" title="Enable Delete Booking to change this setting" @endif>
+                        <input class="form-check-input" type="checkbox" id="customerDeleteBooking" name="customerDeleteBooking" @if($customerDeleteBooking == 1){{ "checked" }}@endif @if($deleteBooking != 1){{ "disabled" }}@endif>
                         <label class="form-check-label" for="customerDeleteBooking">Booking Deleteable by Customer</label>
                         <br>
-                        <small class="@if($deleteBooking == 0){{ 'disabled-label' }}@endif">Enable the customer to delete booking</small>
+                        <small class="@if($deleteBooking != 1){{ 'disabled-label' }}@endif">Enable the customer to delete booking</small>
                     </div>
 
-                    <div class="form-check form-switch" id="bookingDeletableAdmin" @if($deleteBooking == 0) data-bs-toggle="tooltip" data-bs-placement="left" title="Enable Delete Booking to change this setting" @endif>
-                        <input class="form-check-input" type="checkbox" id="adminDeleteBooking" name="adminDeleteBooking" @if($adminDeleteBooking == 1){{ "checked" }}@endif @if($deleteBooking == 0){{ "disabled" }}@endif>
+                    <div class="form-check form-switch" id="bookingDeletableAdmin" @if($deleteBooking != 1) data-bs-toggle="tooltip" data-bs-placement="left" title="Enable Delete Booking to change this setting" @endif>
+                        <input class="form-check-input" type="checkbox" id="adminDeleteBooking" name="adminDeleteBooking" @if($adminDeleteBooking == 1){{ "checked" }}@endif @if($deleteBooking != 1){{ "disabled" }}@endif>
                         <label class="form-check-label" for="adminDeleteBooking">Booking Deleteable by Admin</label>
                         <br>
-                        <small class="@if($deleteBooking == 0){{ 'disabled-label' }}@endif">Enable the admin to delete booking</small>
+                        <small class="@if($deleteBooking != 1){{ 'disabled-label' }}@endif">Enable the admin to delete booking</small>
                     </div>
 
                     <br>
@@ -105,11 +105,11 @@
                         <small>Enable the admin role</small>
                     </div>
 
-                    <div class="form-check form-switch" id="adminSalesReport" @if($adminRole == 0) data-bs-toggle="tooltip" data-bs-placement="left" title="Enable Admin Panel to change this setting" @endif>
-                        <input class="form-check-input" type="checkbox" id="adminSalesReport" name="adminSalesReport" @if($adminSalesReport == 1){{ "checked" }}@endif @if($adminRole == 0){{ "disabled" }}@endif>
+                    <div class="form-check form-switch" id="adminSalesReport" @if($adminRole != 1) data-bs-toggle="tooltip" data-bs-placement="left" title="Enable Admin Panel to change this setting" @endif>
+                        <input class="form-check-input" type="checkbox" id="adminSalesReport" name="adminSalesReport" @if($adminSalesReport == 1){{ "checked" }}@endif @if($adminRole != 1){{ "disabled" }}@endif>
                         <label class="form-check-label" for="adminSalesReport">View Sales Report</label>
                         <br>
-                        <small class="@if($adminRole == 0){{ 'disabled-label' }}@endif">Enable the admin to view sales report</small>
+                        <small class="@if($adminRole != 1){{ 'disabled-label' }}@endif">Enable the admin to view sales report</small>
                     </div>
 
                     <br>
@@ -123,18 +123,18 @@
                         <small>Let customer enjoy different price rate for your specified conditions</small>
                     </div>
 
-                    <div class="form-check form-switch" id="weekdayWeekend" @if($rate == 0) data-bs-toggle="tooltip" data-bs-placement="left" title="Enable Rates to change this setting" @endif>
-                        <input class="form-check-input" type="checkbox" id="weekdayWeekend" name="weekdayWeekend" @if($weekdayWeekend == 1){{ "checked" }}@endif  @if($rate == 0){{ "disabled" }}@endif>
+                    <div class="form-check form-switch" id="weekdayWeekend" @if($rate != 1) data-bs-toggle="tooltip" data-bs-placement="left" title="Enable Rates to change this setting" @endif>
+                        <input class="form-check-input" type="checkbox" id="weekdayWeekend" name="weekdayWeekend" @if($weekdayWeekend == 1){{ "checked" }}@endif  @if($rate != 1){{ "disabled" }}@endif>
                         <label class="form-check-label" for="weekdayWeekend">Weekday Weekend Rate</label>
                         <br>
-                        <small class="@if($rate == 0){{ 'disabled-label' }}@endif">Enable different rate on weekdays and weekends</small>
+                        <small class="@if($rate != 1){{ 'disabled-label' }}@endif">Enable different rate on weekdays and weekends</small>
                     </div>
 
-                    <div class="form-check form-switch" id="adminRates" @if($rate == 0) data-bs-toggle="tooltip" data-bs-placement="left" title="Enable Rates to change this setting" @endif>
-                        <input class="form-check-input" type="checkbox" id="adminRates" name="adminRates" @if($adminRates == 1){{ "checked" }}@endif  @if($rate == 0){{ "disabled" }}@endif>
+                    <div class="form-check form-switch" id="adminRates" @if($rate != 1) data-bs-toggle="tooltip" data-bs-placement="left" title="Enable Rates to change this setting" @endif  @if($adminRole != 1) data-bs-toggle="tooltip" data-bs-placement="left" title="Enable Admin Panel to change this setting" @endif>
+                        <input class="form-check-input" type="checkbox" id="adminRates" name="adminRates" @if($adminRates == 1){{ "checked" }}@endif  @if($rate != 1){{ "disabled" }}@endif  @if($adminRole != 1){{ "disabled" }}@endif>
                         <label class="form-check-label" for="adminRates">Rates Editable by Admin</label>
                         <br>
-                        <small class="@if($rate == 0){{ 'disabled-label' }}@endif">Enable the admin to edit rates detail</small>
+                        <small class="@if($rate != 1){{ 'disabled-label' }}@endif">Enable the admin to edit rates detail</small>
                     </div>
 
                 </div>
