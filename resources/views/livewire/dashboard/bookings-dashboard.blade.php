61248852b3aeb0cff892a74d42d7f729c70f0a60
Wei Jie Jie <>
2021-10-20 21:30:43 +0800

    Future feature extension for bookings
    - migrate static content to main blade
    - booking details modal now available

--- resources/views/livewire/dashboard/bookings-dashboard.blade.php
+++ resources/views/livewire/dashboard/bookings-dashboard.blade.php
@@ -37,118 +37,6 @@
         }
 
         @endfor
-
-        .event,
-        .event-group {
-            grid-column: var(--court) 1;
-            grid-row: var(--start-time)/var(--end-time);
-            z-index: 1;
-        }
-
-        .event-group {
-            display: flex;
-        }
-
-        .event-group .event {
-            flex: 1;
-        }
-
-        .court {
-            grid-column: var(--court) 1;
-            grid-row: 1/2;
-            position: sticky;
-            top: 0;
-            z-index: 2;
-        }
-
-        .time {
-            grid-column: time 1;
-            grid-row: var(--start-time) 1;
-            position: sticky;
-            left: 0;
-            z-index: 2;
-        }
-
-        .highlight-day {
-            grid-column: var(--court) 1;
-            grid-row: 1/-1;
-            position: relative;
-        }
-
-        .highlight-time,
-        .current-time {
-            grid-column: 1/-1;
-            grid-row: var(--start-time) 1;
-            position: relative;
-            left: 0;
-            right: 0;
-        }
-
-        .current-time {
-            z-index: 2;
-            height: 1px;
-            background: red;
-        }
-
-        .event {
-            margin: 2px;
-            padding: 0.5em;
-            background: white;
-            box-shadow: 0 0 8px rgba(0, 0, 0, 0.1), 0 2px 2px rgba(0, 0, 0, 0.1);
-            border-radius: 4px;
-        }
-
-        .court {
-            text-align: center;
-            background: white;
-            padding: 1em;
-            border-bottom: 1px solid rgba(0, 0, 0, 0.1);
-        }
-
-        .time {
-            background: white;
-            padding: 4px 1em;
-            font-size: 0.75em;
-            color: #AAA;
-            border-right: 1px solid rgba(0, 0, 0, 0.1);
-        }
-
-        .highlight-day {
-            background: rgba(0, 0, 0, 0.03);
-        }
-
-        .highlight-time {
-            background: rgba(0, 0, 0, 0.03);
-        }
-
-        .calendar {
-            margin: 0 auto;
-        }
-
-        .today {
-            font-weight: bold;
-            background-color: lightyellow;
-        }
-
-
-        /* css below is for the date selector */
-        .selection {
-            align-self: baseline;
-        }
-
-        /* css below is part of bookings modal inner workings */
-        .indicator {
-            display: none;
-        }
-
-        .amandable:hover {
-            background: #f5f5f5;
-            cursor: pointer;
-        }
-
-        .event:hover .indicator {
-            display: block;
-        }
     </style>
 
     <div class="row align-items-center justify-content-end">
@@ -163,7 +51,7 @@
         </div>
 
         <div class="col-auto">
-            <input type="date" class="form-control mb-1" id="dateSlot" name="dateSlot" required wire:model="date">
+            <input type="date" class="form-control mb-1" id="dateSlot" name="dateSlot" required wire:model="date" value="{{ $date }}">
         </div>
     </div>
 
@@ -190,7 +78,9 @@
                 <!-- Events -->
                 @foreach ($bookings as $booking)
                     <div style="display: flex; justify-content: center; align-items: center;" class="event @if(($booking->dateSlot >= date('Ymd') && $booking->timeSlot > date('H')) || $booking->dateSlot > date('Ymd')){{ 'amandable' }}@endif
-                     {{ "court".$booking->courtID }} from-{{ $booking->timeSlot }} to-{{ $booking->timeSlot + $booking->timeLength }}">
+                     {{ "court".$booking->courtID }} from-{{ $booking->timeSlot }} to-{{ $booking->timeSlot + $booking->timeLength }}"
+                     data-bs-toggle="modal" data-bs-target="#bookingDetailsModal" data-name="{{ $booking->name }}" data-date="{{ substr($booking->dateSlot, 6, 2) . '/' . substr($booking->dateSlot, 4, 2) . '/' . substr($booking->dateSlot, 0, 4) }}" data-time="{{ $booking->timeSlot.':00' }}" data-price="{{ $booking->bookingPrice * $booking->timeLength }}"
+                     >
                         {{ $booking->bookingRateName }}
                         @if(($booking->dateSlot >= date('Ymd') && $booking->timeSlot > date('H')) || $booking->dateSlot > date('Ymd'))
                         <span class="indicator">&nbsp<i class="bi bi-arrow-up-right-circle"></i></span>
--- resources/views/manager/bookings.blade.php
+++ resources/views/manager/bookings.blade.php
@@ -5,15 +5,121 @@ Bookings
 @endsection
 
 @section('extra-dependencies')
-<script src="{{ URL::asset('dependencies/admin/courtBookingsTableSearch.js') }}"></script>
 <script src="{{ URL::asset('dependencies/sortable-0.8.0/js/sortable.min.js') }}"></script>
 @endsection
 
 @section('extra-css')
 <style>
-    th {
-        cursor: pointer;
-    }
+    .event,
+        .event-group {
+            grid-column: var(--court) 1;
+            grid-row: var(--start-time)/var(--end-time);
+            z-index: 1;
+        }
+
+        .event-group {
+            display: flex;
+        }
+
+        .event-group .event {
+            flex: 1;
+        }
+
+        .court {
+            grid-column: var(--court) 1;
+            grid-row: 1/2;
+            position: sticky;
+            top: 0;
+            z-index: 2;
+        }
+
+        .time {
+            grid-column: time 1;
+            grid-row: var(--start-time) 1;
+            position: sticky;
+            left: 0;
+            z-index: 2;
+        }
+
+        .highlight-day {
+            grid-column: var(--court) 1;
+            grid-row: 1/-1;
+            position: relative;
+        }
+
+        .highlight-time,
+        .current-time {
+            grid-column: 1/-1;
+            grid-row: var(--start-time) 1;
+            position: relative;
+            left: 0;
+            right: 0;
+        }
+
+        .current-time {
+            z-index: 2;
+            height: 1px;
+            background: red;
+        }
+
+        .event {
+            margin: 2px;
+            padding: 0.5em;
+            background: white;
+            box-shadow: 0 0 8px rgba(0, 0, 0, 0.1), 0 2px 2px rgba(0, 0, 0, 0.1);
+            border-radius: 4px;
+        }
+
+        .court {
+            text-align: center;
+            background: white;
+            padding: 1em;
+            border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+        }
+
+        .time {
+            background: white;
+            padding: 4px 1em;
+            font-size: 0.75em;
+            color: #AAA;
+            border-right: 1px solid rgba(0, 0, 0, 0.1);
+        }
+
+        .highlight-day {
+            background: rgba(0, 0, 0, 0.03);
+        }
+
+        .highlight-time {
+            background: rgba(0, 0, 0, 0.03);
+        }
+
+        .calendar {
+            margin: 0 auto;
+        }
+
+        .today {
+            font-weight: bold;
+            background-color: lightyellow;
+        }
+
+        /* css below is for the date selector */
+        .selection {
+            align-self: baseline;
+        }
+
+        /* css below is part of bookings modal inner workings */
+        .indicator {
+            display: none;
+        }
+
+        .amandable:hover {
+            background: #f5f5f5;
+            cursor: pointer;
+        }
+
+        .event:hover .indicator {
+            display: block;
+        }
 </style>
 @endsection
 
@@ -21,6 +127,26 @@ Bookings
 <div class="container">
     @livewire('dashboard.bookings-dashboard')
 </div>
+
+<!-- Modal -->
+<div class="modal fade" id="bookingDetailsModal" tabindex="-1" aria-labelledby="bookingDetailsModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="bookingDetailsModalLabel">Booking Details</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>Customer Name: <span id="name"></span></p>
+                <p>Date: <span id="date"></span></p>
+                <p>Time: <span id="time"></span></p>
+                <p>Paid: RM <span id="paid"></span></p>
+            </div>
+            {{-- <div class="modal-footer"></div> --}}
+        </div>
+    </div>
+</div>
+  
 @endsection
 
 @section('bottom-js')
@@ -28,6 +154,13 @@ Bookings
     $(document).ready(function() {
         document.getElementById("dateSlot").value = "{{ date('Y-m-d') }}"
         $("#dateSlot").change()
+    })
+
+    $(document).on("click", ".amandable", function() {
+        $("#name").text($(this).data('name'))
+        $("#date").text($(this).data('date'))
+        $("#time").text($(this).data('time'))
+        $("#paid").text($(this).data('price'))
     })
 </script>
 @endsection
