76594e1ea73cb8bc157b7a90137d5a5c89a6448a
Wei Jie Jie <>
2021-10-26 23:16:23 +0800

    Updated manager check in interface

--- app/Http/Controllers/Manager/ManagerCheckInController.php
+++ app/Http/Controllers/Manager/ManagerCheckInController.php
@@ -18,7 +18,7 @@ class ManagerCheckInController extends Controller
     function view()
     {
 
-        return view('manager.checkin', ['result' => null]);
+        return view('manager.checkin', ['result' => "0"]);
     }
 
     function check(Request $request)
@@ -36,6 +36,72 @@ class ManagerCheckInController extends Controller
             ->where('custID', '=', substr($request->input('resultToQuery'), 8, 7))
             ->first();
 
-        return view('manager.checkin', ['resultToQuery' => $request->resultToQuery, 'currentTime' => date('H'), 'currentDate' => date('Ymd'), 'result' => $result]);
+        $currentTime = date('H');
+        $currentDate = date('Ymd');
+
+        if ($result == null) {
+
+            // invalid booking ID
+            $cardColor = "danger";
+            $cardIcon = "bi-x-circle-fill";
+            $cardText = "Invalid Booking ID";
+
+        } else if ($currentDate == $result->dateSlot) {
+            // if date was today
+
+            if ($currentTime >= $result->timeSlot && $currentTime <= ($result->timeSlot + $result->timeLength - 1)) {
+
+                // valid booking
+                $cardColor = "success";
+                $cardIcon = "bi-check-circle-fill";
+                $cardText = "Valid Booking";
+            } else if ($currentTime < $result->timeSlot) {
+
+                // the current check in came too early today
+                $cardColor = "info";
+                $cardText = "Future Book Slot, Came Too Early";
+                $cardIcon = "bi-brightness-alt-high";
+            } else if ($currentTime > ($result->timeSlot + $result->timeLength)) {
+
+                // the current check in came too late today
+                $cardColor = "warning";
+                $cardIcon = "bi-watch";
+                $cardText = "Came Too Late, Book Slot Expired";
+            } else {
+
+                // the current check in came too late today
+                $cardColor = "warning";
+                $cardIcon = "bi-watch";
+                $cardText = "Came Too Late, Book Slot Expired";
+            }
+        } else {
+            if ($currentDate < $result->dateSlot) {
+
+                // the current check in came too early
+                $cardColor = "info";
+                $cardText = "Future Book Slot, Came Too Early";
+                $cardIcon = "bi-brightness-alt-high";
+            } else if ($currentDate > $result->dateSlot) {
+
+                // the current check in has expired (will be shown when it was older than today)
+                $cardColor = "danger";
+                $cardIcon = "bi-watch";
+                $cardText = "Expired Book Slot";
+            } else {
+
+                // error if nothing catches the error
+                $cardColor = "danger";
+                $cardIcon = "bi-cone-striped";
+                $cardText = "System Error";
+            }
+        }
+
+        return view('manager.checkin', [
+            'resultToQuery' => $request->resultToQuery,
+            'result' => $result,
+            'cardColor' => $cardColor,
+            'cardIcon' => $cardIcon,
+            'cardText' => $cardText,
+        ]);
     }
 }
--- resources/views/manager/checkin.blade.php
+++ resources/views/manager/checkin.blade.php
@@ -38,166 +38,107 @@ Check In
 @endsection
 
 @section('body')
-<div class="container">
+    <div class="container">
 
-    <!-- scanner/camera preview window -->
-    <div class="video-container row justify-content-md-center" style="display: none">
-        <video id="video-preview"></video>
-        <canvas id="qr-canvas" style="display: none;"></canvas>
-    </div>
+        <div class="row align-items-center">
+    
+            <div class="col">
+    
+                <!-- scanner/camera preview window -->
+                <div class="video-container row justify-content-center">
+                    <video id="video-preview"></video>
+                    <canvas id="qr-canvas" style="display: none;"></canvas>
+                </div>
+    
+                <br>
 
-    <br>
-
-    <!-- submission field -->
-    <div class="row justify-content-md-center">
-        @error('resultToQuery')
-        <div class="alert alert-danger" role="alert">
-            {{ $message }}
-        </div>
-        @enderror
-    </div>
-
-    <div class="row justify-content-md-center">
-        <div class="col-auto">
-            <form action="{{ route('manager.checkin') }}" method="post">
-                @csrf
-                <input id="resultToQuery" class="form-control" name="resultToQuery" type="text" placeholder="Scan or type check in code" value="" minlength=14 maxlength="14" required>
-        </div>
-        <div class="col-auto">
-            <button class="btn btn-primary" type="submit" id="startQuery" name="startQuery">
-                <i class="bi bi-person-check-fill"></i>
-                Check-in
-            </button>
-            </form>
-        </div>
-    </div>
-
-    <br>
-
-    @if($result == null)
-    @php
-        // invalid booking ID
-        $cardColor = "danger";
-        $cardIcon = "bi-x-circle-fill";
-        $cardText = "Invalid Booking ID";
-    @endphp
-    @elseif ($currentDate == $result->dateSlot)
-        {{-- if date was today --}}
-
-        @if($currentTime >= $result->timeSlot && $currentTime <= ($result->timeSlot + $result->timeLength - 1))
-            @php
-                // valid booking
-                $cardColor = "success";
-                $cardIcon = "bi-check-circle-fill";
-                $cardText = "Valid Booking";
-            @endphp
-        @elseif($currentTime < $result->timeSlot)
-            @php
-                // the current check in came too early today
-                    $cardColor = "info";
-                $cardText = "Future Book Slot, Came Too Early";
-                $cardIcon = "bi-brightness-alt-high";
-            @endphp
-        @elseif($currentTime > ($result->timeSlot + $result->timeLength))
-            @php
-                // the current check in came too late today
-                $cardColor = "warning";
-                $cardIcon = "bi-watch";
-                $cardText = "Came Too Late, Book Slot Expired";
-            @endphp
-        @else
-            @php
-                // the current check in came too late today
-                $cardColor = "warning";
-                $cardIcon = "bi-watch";
-                $cardText = "Came Too Late, Book Slot Expired";
-            @endphp
-        @endif
-    @else
-        @if($currentDate < $result->dateSlot)
-            @php
-                // the current check in came too early
-                $cardColor = "info";
-                $cardText = "Future Book Slot, Came Too Early";
-                $cardIcon = "bi-brightness-alt-high";
-            @endphp
-        @elseif($currentDate > $result->dateSlot)
-            @php
-                // the current check in has expired (will be shown when it was older than today)
-                $cardColor = "danger";
-                $cardIcon = "bi-watch";
-                $cardText = "Expired Book Slot";
-            @endphp
-        @else
-            @php
-                // error if nothing catches the error
-                $cardColor = "danger";
-                $cardIcon = "bi-cone-striped";
-                $cardText = "System Error";
-            @endphp
-        @endif
-    @endif
-
-    @if(!$result == null)
-    <!-- result card -->
-    <div class="card">
-        <div class="text-white card bg-{{ $cardColor }}">
-            <div class="card-body">
-                <span class="text-centre">
-                    <h1 class="card-title">
-                        <i style="font-size: 3rem;" class="bi {{ $cardIcon }}"></i>
-                        {{ $cardText }}
-                    </h1>
-                </span>
+                <!-- submission field -->
+                <div class="row justify-content-center">
+                    @error('resultToQuery')
+                        <div class="alert alert-danger" role="alert">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+    
+                <div class="row justify-content-center">
+                    <div class="col">
+                        <form action="{{ route('manager.checkin') }}" method="post">
+                            @csrf
+                            <input id="resultToQuery" class="form-control" name="resultToQuery" type="text"
+                                placeholder="Scan or type check in code" value="" minlength=14 maxlength="14" required>
+                    </div>
+                    <div class="col-auto">
+                        <button class="btn btn-primary" type="submit" id="startQuery" name="startQuery">
+                            <i class="bi bi-person-check-fill"></i>
+                            Check-in
+                        </button>
+                        </form>
+                    </div>
+                </div>
+    
+                <br>
+    
             </div>
+    
+            <div class="col-xl">
+    
+                @if ($result != '0')
+                    <!-- result card -->
+                    <div class="card">
+                        <div class="text-white card bg-{{ $cardColor }}">
+                            <div class="card-body">
+                                <span class="text-centre">
+                                    <h1 class="card-title">
+                                        <i style="font-size: 3rem;" class="bi {{ $cardIcon }}"></i>
+                                        {{ $cardText }}
+                                    </h1>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+    
+                    <br>
+                @endif
+    
+                @if (!$result == null && $result != '0')
+                    <!-- result detail card -->
+                    <div class="card bg-light">
+                        <div class="card-body">
+                            <table class="table">
+                                <tr>
+                                    <td>Time</td>
+                                    <td>{{ substr($result->dateSlot, 6, 2) }}/{{ substr($result->dateSlot, 4, 2) }}/{{ substr($result->dateSlot, 0, 4) }}
+                                        {{ $result->timeSlot }}:00 -
+                                        {{ $result->timeSlot + $result->timeLength }}:00</td>
+                                </tr>
+                                <tr>
+                                    <td>Rate</td>
+                                    <td>
+                                        {{ $result->bookingRateName }}
+                                        @if ($result->condition)
+                                            <br>
+                                            {{ $result->condition }}
+                                        @endif
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Court</td>
+                                    <td>{{ $result->courtID }}</td>
+                                </tr>
+                                <tr>
+                                    <td>Name</td>
+                                    <td>{{ $result->name }}</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                @endif
+                <br>
+    
+            </div>
+
         </div>
+
     </div>
-
-    <br>
-
-    <!-- result detail card -->
-    <div class="card bg-light">
-        <div class="card-body">
-            <h2 class="card-title">
-                Booking Details
-            </h2>
-            <table class="table">
-                <tbody>
-                    <tr>
-                        <td>Booking ID</td>
-                        <td>{{ substr($resultToQuery, 0, 7) }}</td>
-                    </tr>
-                    <tr>
-                        <td>Time</td>
-                        <td>{{ substr($result -> dateSlot, 6, 2) }}/{{ substr($result -> dateSlot, 4, 2)}}/{{ substr($result -> dateSlot, 0, 4) }} {{ $result -> timeSlot }}:00 - {{ ($result -> timeSlot + $result -> timeLength) }}:00</td>
-                    </tr>
-                    <tr>
-                        <td>Length</td>
-                        <td>{{ $result -> timeLength }} hours</td>
-                    </tr>
-                    <tr>
-                        <td>Rate</td>
-                        <td>
-                            {{ $result -> bookingRateName }}
-                            @if($result->condition)
-                                <br> <p class="h4">{{ $result->condition }}</p>
-                            @endif
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Customer Name</td>
-                        <td>{{ $result -> name }}</td>
-                    </tr>
-                    <tr>
-                        <td>Court</td>
-                        <td>{{ $result -> courtID }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-    @endif
-</div>
-
-<br>
 @endsection
